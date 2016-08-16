9b382ba766e3651bd8d0cb67fba4a13f82d22df6
magnus <magnus@genesilico.pl>
2016-08-16 18:46:31 +0200

    fix to --get_rnapuzzle

--- test.sh
+++ test.sh
@@ -18,16 +18,16 @@
 
 ./yapdb_parser.py --nohr --clean input/1xjr_GTP.pdb > output/1xjr_GTP.pdb
 
-./yapdb_parser.py --nohr --getrnapuzzle input/1xjr_onlyGTP.pdb
+./yapdb_parser.py --nohr --get_rnapuzzle input/1xjr_onlyGTP.pdb
 
-./yapdb_parser.py --nohr --getrnapuzzle input/1xjr_onlyGTP.pdb > output/1xjr_onlyGTP_rnapuzzle_ready.pdb
+./yapdb_parser.py --nohr --get_rnapuzzle input/1xjr_onlyGTP.pdb > output/1xjr_onlyGTP_rnapuzzle_ready.pdb
 
 ./yapdb_parser.py --nohr --clean input/1osw.pdb > output/1osw_NMR_1.pdb
 
-./yapdb_parser.py --nohr --getrnapuzzle input/4GXY_3firstNt.pdb
+./yapdb_parser.py --nohr --get_rnapuzzle input/4GXY_3firstNt.pdb
 
-./yapdb_parser.py --nohr --getrnapuzzle input/gtp.pdb  > output/gtp.pdb
+./yapdb_parser.py --nohr --get_rnapuzzle input/gtp.pdb  > output/gtp.pdb
 
-./yapdb_parser.py --nohr --getrnapuzzle input/377D.pdb # should finish with error
+./yapdb_parser.py --nohr --get_rnapuzzle input/377D.pdb # should finish with error
 
 ./yapdb_parser.py --nohr --get_simrna_ready input/1xjr_no_op3.pdb > output/1xjr_no_op3_simrna_ready.pdb
