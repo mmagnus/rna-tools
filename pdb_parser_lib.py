14cb2800031c1016b533f33dba5355ceadac1103
magnus <magnus@genesilico.pl>
2016-08-19 21:32:40 +0200

    Add (draft) edit_occupancy_of_pdb(txt, pdb, pdb_out,v=False):

--- pdb_parser_lib.py
+++ pdb_parser_lib.py
@@ -789,6 +789,58 @@ class StrucFile:
         s = StrucFile(fout)
         self.lines = s.lines
 
+    def edit_occupancy_of_pdb(txt, pdb, pdb_out,v=False):
+        """Make all atoms 1 (flexi) and then set occupancy 0 for seletected atoms.
+        Return False if error. True if OK
+        """
+        struc = PDB.PDBParser().get_structure('struc', pdb)
+
+        txt = txt.replace(' ','')
+        if v:print txt
+        l = re.split('[,:;]', txt)
+        if v:print l 
+
+        for s in struc:
+            for c in s:
+                for r in c:
+                    for a in r:
+                        a.set_occupancy(1)  # make it flaxi
+
+        for i in l: # ['A', '1-10', '15', '25-30', 'B', '1-10']
+
+            if i in string.ascii_letters:
+                if v:print 'chain', i
+                chain_curr = i
+                continue
+
+            if i.find('-') > -1:
+                start, ends = i.split('-')
+                if start > ends:
+                    print >>sys.stderr, 'Error: range start > end ' + i
+                    return False
+                index = range(int(start), int(ends)+1)
+            else:
+                index=[int(i)]
+
+            for i in index:
+                # change b_factor
+                try:
+                    atoms = struc[0][chain_curr][i]
+                except KeyError:
+                    if i == chain_curr:
+                        print >>sys.stderr, 'Error: Chain ' + chain_curr + ' not found in the PDB structure'
+                    else:
+                        print >>sys.stderr, 'Error: Residue ' + chain_curr + ':' + str(i) + ' found in the PDB structure'
+                        return False
+                for a in atoms:
+                    a.set_occupancy(0)
+
+        io = PDBIO()
+        io.set_structure(struc)
+        io.save(pdb_out)
+        print 'Saved ', pdb_out
+        return True
+
 def start(): pass
 
 if '__main__' == __name__:
