00dcb5c451f48ceb62094e2542658e1aa6aff8e2
magnus <magnus@genesilico.pl>
2016-08-23 18:27:39 +0200

    Add various rmsd_calc scripts based on biopython (https://github.com/biopython/biopython) and rmsd (https://github.com/charnley/rmsd)

---
+++ utils/rmsd_calc/test.sh
@@ -0,0 +1,14 @@
+cmd="./rmsd_calc_biopy_dir.py -i test_data -o test_output/rmsd_calc_biopy_dir_matrix.tsv"
+echo $cmd
+$cmd
+echo
+
+cmd="./rmsd_calc_dir.py -i test_data -o test_output/rmsd_calc_dir.tsv"
+echo $cmd
+$cmd
+echo
+
+cmd="./rmsd_calc_dir_to_target.py -t test_data/struc1.pdb -i test_data -o test_output/rmsd_calc_dir_to_target.tsv"
+echo $cmd
+$cmd
+echo
