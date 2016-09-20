5000a069b0a7ab32eefafc7acef64946450837cb
magnus <magnus@genesilico.pl>
2016-09-20 22:19:00 +0200

    Test install numpy and biopython for RTD

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      packages=[''],
+      install_requires=[
+          'numpy',
+          'biopython'
+      ],
+      zip_safe=False)
