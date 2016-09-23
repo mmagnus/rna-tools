f2b4d676de9845c1188f421b9dd2820db322c88f
magnus <magnus@genesilico.pl>
2016-09-23 20:59:56 +0200

    Ignore bin and update the install script

--- install_links_bin.sh
+++ install_links_bin.sh
@@ -3,5 +3,7 @@
 rm bin/*
 curr_dir=$(pwd)
 ln -s $curr_dir/rna_pdb_tools/utils/rmsd_calc/rmsd_calc_to_target.py $curr_dir/bin/rmsd_calc_to_target.py
+ln -s $curr_dir/rna_pdb_tools/rna-pdb-tools.py $curr_dir/bin/rna-pdb-tools.py
+ln -s $curr_dir/rna_pdb_tools/utils/diffpdb/diffpdb.py $curr_dir/bin/diffpdb
 echo 'Installed in ./bin'
-ls bin
+ls -l bin
