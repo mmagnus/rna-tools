3dfa486c4123991eb17b625b3beb581a5f7ba121
magnus <magnus@genesilico.pl>
2016-09-27 19:49:48 +0200

    simrna_trajecotry: try to opti.. load_from_file vs load_from_string

--- rna_pdb_tools/utils/simrna_trajectory/simrna_trajectory.py
+++ rna_pdb_tools/utils/simrna_trajectory/simrna_trajectory.py
@@ -5,24 +5,37 @@
 from collections import deque
 import numpy as np
 
+import gc
+
+
 class SimRNATrajectory:
     """SimRNATrajectory:
 
     -> Frame / Residue / Atom"""
-    def __init__(self, fn):
+    def __init__(self):
+        self.frames = []
+        
+    def load_from_file(self, fn):
         """h(eader), l(line), f(ile)"""
         frames = []
-        f = open(fn)
+        f = (line for line in open(fn).xreadlines())
         c = 1
-        h = f.readline().strip()
-        l = f.readline().strip()
+        h = f.next().strip()
+        l = f.next().strip()
         frames.append(Frame(c, h, l))
-        while h:
+        c = 0
+        while 1:
             c += 1
-            h = f.readline().strip()
-            l = f.readline().strip()
+            try:
+                h = f.next().strip()
+            except StopIteration:
+                break
+            l = f.next().strip()
             if h and l:
                 frames.append(Frame(c, h, l))
+                if c % 1000 == 0:
+                    print c/1000,'k cleaning...'
+                    gc.collect()
         self.frames = frames
         #with open(fn) as f:
         #    for line in f:
@@ -30,6 +43,11 @@ class SimRNATrajectory:
         #f = open(fn)  # how much is loaded in memory?
         #f.next()  
 
+    def load_from_string(self, txt):
+        c = 0
+        h,l = txt.split('\n')
+        self.frames.append(Frame(c, h, l))        
+    
 class Atom:
     """Atom"""
     def __init__(self, name, x, y, z):
@@ -48,6 +66,7 @@ class Atom:
     def __sub__(self, other_atom):
         diff = self.coord - other_atom.coord
         return np.sqrt(np.dot(diff, diff)) 
+
     def __add__(self, other_atom):
         return self.coord + other_atom.coord
 
@@ -62,6 +81,7 @@ class Residue:
         self.b2 = b2
         self.atoms = [p, c4p, n1n9, b1, b2]
         # self.center
+
     def __repr__(self):
         return 'Residue ' + str(self.id)
 
@@ -97,7 +117,8 @@ class Frame:
                 
 #main
 if __name__ == '__main__':
-    s = SimRNATrajectory('8b2c1278-ee2f-4ca2-bf4a-114ec7151afc_ALL_thrs6.20A_clust01.trafl')
+    s = SimRNATrajectory()
+    s.load_from_file('8b2c1278-ee2f-4ca2-bf4a-114ec7151afc_ALL_thrs6.20A_clust01.trafl')
     for f in s.frames:
         #print f.header
         #print f.coords
@@ -111,3 +132,11 @@ if __name__ == '__main__':
         print r.b2
         print r.get_center()
         break
+
+
+    s2 = SimRNATrajectory()
+    traj = """1 1 1252.257530 1252.257530 0.950000
+ 53.570 23.268 39.971 55.119 24.697 43.283 55.145 27.966 42.270 55.258 29.321 42.618 54.313 29.909 40.572 57.246 41.229 41.492 57.056 39.572 45.104 55.672 36.799 43.722 55.491 33.284 44.069 55.013 33.922 41.769"""
+    s2.load_from_string(traj)
+    for f in s2.frames:
+        print f
