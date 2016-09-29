d6c4abb39b1acb51e8d1f56407aaaa7dd0243d9c
magnus <magnus@genesilico.pl>
2016-09-29 18:46:21 +0200

    PyMOL4RNA: merge with rna-pdb-tools

---
+++ rna_pdb_tools/utils/PyMOL4RNA/PyMOL4RNA.py
@@ -0,0 +1,27 @@
+def color_by_text(txt):
+  for t in txt.strip().split('\n'):
+    color, resi = t.replace('color ', '').split(',')
+    print color, resi
+    cmd.color(color.strip(), resi.strip())
+    
+def rp():
+    """rna like in papers ;-)"""
+    cmd.hide("sticks", "all")
+    cmd.hide("lines", "all")
+    cmd.show("cartoon", "all")
+    cmd.set("cartoon_ring_mode", 3)
+    cmd.set("cartoon_ring_finder", 2)
+    
+def rp17():
+    txt ="""color forest, resi 1-5+12-16; # p1
+ color magenta, resi 6-11+34-39
+ color grey, resi 17-24
+ color marine, resi 25-28+55-58 
+ color deepblue, resi 29-33+40-42
+ color orange, resi 42-47+48-52;
+ color yellow, resi 53-54;
+"""
+    color_by_text(txt)
+
+cmd.extend('rp17', rp17)
+cmd.extend('rp', rp)   
---
+++ rna_pdb_tools/utils/PyMOL4RNA/README.md
@@ -0,0 +1,29 @@
+PyMOL4RNA
+=========
+
+To get:
+
+![rna](doc/rna.png)
+
+(install the plugin) and run:
+
+	PyMOL>rp
+	PyMOL>rp17
+	
+The source of the pdb: <http://www.rcsb.org/pdb/explore/explore.do?structureId=5K7C> see also `test_data/5k7c_clean.pdb`
+
+Install
+-------------------------------------------------------------------------------
+
+Add:
+
+    sys.path.append('<path to the plugin>/PyMOL4RNA/')
+    run <path to the plugin>/PyMOL4RNA/PyMOL4RNA.py
+
+e.g.:
+
+    sys.path.append('/Users/magnus/Dropbox/work/workspace/PyMOL4RNA/')
+    run /Users/magnus/Dropbox/work/workspace/PyMOL4RNA/PyMOL4RNA.py
+
+to our `~/.pymolrc`
+
