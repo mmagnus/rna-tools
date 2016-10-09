46df5a237bb6890fbdc9bc72c0228d086babd218
magnus <magnus@genesilico.pl>
2016-10-10 00:30:20 +0200

    add small function to get numbering

---
+++ rna_pdb_tools/utils/extra_functions/gen_numbering.py
@@ -0,0 +1,17 @@
+def seq_numbers():
+    """
+    output:
+    - |0.......|10......|20......|30......|40......|50......|60......|70......|80......|90......|0.......|10......|20......|30......|40......|50......|60......|70
+    """
+    c=0
+    f=c*100
+    s=c*100+100
+    lineNr='|1.......'#beginning of the line
+    for i in [10,20,30,40,50,60,70,80,90]:
+            lineNr+= '|' + str(i)+('.'*(9-len(str(i))))
+    lineNr = lineNr + lineNr + lineNr
+    return lineNr
+
+if __name__ == '__main__':
+    print seq_numbers()
+    
