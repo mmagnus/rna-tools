b07eaa485aa6aa420c71caa4d9ae240fd9a8c04f
calmeida <calmeida@genesilico.pl>
2016-10-19 11:50:37 +0200

    Udgrade bpseq2cl.R

--- bpseq2cl.R
+++ bpseq2cl.R
@@ -28,10 +28,12 @@ df$value <- rep(round(1, digits=2),nrow(bpseq))
 df$posi <- bpseq$posi
 df$posj <- bpseq$posj
 df$residuei <- bpseq$res
+df$residuei <- as.character(df$residuei)
 
 # To keep only the resi and resj that actually form a connection
 df <- df[!df$posj == 0, ] 
 df$residuej <- bpseq$res[bpseq$posj[1:nrow(bpseq)]]
+df$residuej <- as.character(df$residuej)
 
 # To remove repeated connections. Exemple: resi=6 and resj=378 are connected, as are resi=378 with resj=6. We don't need the repetition.
 cols = c(2,4) 
@@ -41,6 +43,11 @@ for (i in 1:nrow(df))
   newdf[i, ] = sort(df[i,cols])
 }
 df <- df[!duplicated(newdf),]
+
+# clarna_compare.py has as first line "Classifier: Clarna", so this line must be appended
+df <- rbind(c("Classifier: Clarna", "", "", "", "", "", "", "", ""), df)
 df
 
 write.table(df, file = paste(args[1],"_CLnotation.txt", sep = ""), col.names = F, row.names = F, sep = " ")
+
+# Before running clarna_compare.py on your files, remove all "
