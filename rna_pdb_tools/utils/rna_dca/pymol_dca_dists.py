03eed8da3047c3a6bc1ae7e34a7c71b4aa5e183a
magnus <magnus@genesilico.pl>
2016-10-21 16:02:24 +0200

    remove DCA (keep it as a seperate project)

--- rna_pdb_tools/utils/rna_dca/pymol_dca_dists.py
+++
@@ -1,7 +0,0 @@
-def get_dists(interactions): # l=([1,2], [3,4])
-    for i in interactions:
-        a = "////" + str(i[0]) + "/C2'"
-        b = "////" + str(i[1]) + "/C2'"
-        cmd.distance('d'+str(i[0])+'-'+str(i[1]),"(" + a + ")", "(" + b + ")")# mode, 4)
-
-cmd.extend('get_dists', get_dists)
--- rna_pdb_tools/utils/rna_dca/test.sh
+++
@@ -1,1 +0,0 @@
-python rna_dca_mapping.py examples/1fir_gapped.fa examples/rf5.scores_parsed.csv > examples/rna_dca_mapping_output.txt
