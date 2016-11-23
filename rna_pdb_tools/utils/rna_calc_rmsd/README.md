35105f22766ef5b0e52579c1fd4be99dcc9288bd
magnus <magnus@genesilico.pl>
2016-11-23 18:21:02 +0100

    rna_calc_rmsd: huge refactor

---
+++ rna_pdb_tools/utils/rna_calc_rmsd/README.md
@@ -0,0 +1,80 @@
+rna_calc_rmsd.py
+-------------------------------------------------------------------------------
+
+Using PyMOL align:
+
+	./rna_calc_rmsd.py -m align -t test_data/struc1.pdb -o test_output/rmsd_calc_dir_to_target.tsv test_data/struc1.pdb test_data/struc2.pdb test_data/struc3.pdb test_data/struc4.pdb
+	rmsd_calc_rmsd_to_target
+	--------------------------------------------------------------------------------
+	method: align
+	# of models: 4
+	struc1.pdb 0.0 1321
+	struc2.pdb 11.8030576706 1321
+	struc3.pdb 4.87882900238 1321
+	struc4.pdb 3.98158359528 1321
+	# of atoms used: 1321
+	csv was created!  test_output/rmsd_calc_dir_to_target.tsv
+
+or by default full atom rmsd with the internal lib:
+
+	$ ./rna_calc_rmsd.py -t test_data/struc1.pdb -o test_output/rmsd_calc_dir_to_target.tsv test_data/struc1.pdb test_data/struc2.pdb test_data/struc3.pdb test_data/struc4.pdb
+	rmsd_calc_rmsd_to_target
+	--------------------------------------------------------------------------------
+	# of models: 4
+	target:struc1.pdb      	rmsd_all
+	struc1.pdb     	0.0
+	struc2.pdb     	11.803
+	struc3.pdb     	4.879
+	struc4.pdb     	3.982
+	tsv was created!  test_output/rmsd_calc_dir_to_target.tsv
+	
+.. select some residues and ignore some atoms 
+
+    $ ./rna_calc_rmsd.py -t test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb --target_selection A:1-47+52-62 --model_selection A:1-47+52-62 /Users/magnus/work/src/rna-pdb-tools/rna_pdb_tools/utils/rna_calc_rmsd/test_data/pistol/clusters/pistol_thrs0.50A_clust01-000001_AA.pdb --model_ignore_selection A/57/O2\'  test_data/pistol/clusters/*_AA.pdb
+    
+    rmsd_calc_rmsd_to_target
+    --------------------------------------------------------------------------------
+     method:
+    # of models: 4
+    pistol_thrs0.50A_clust01-000001_AA.pdb 7.59648219355 1237
+    pistol_thrs0.50A_clust01-000001_AA.pdb 7.59648219355 1237
+    pistol_thrs0.50A_clust02-000001_AA.pdb 7.76647001671 1237
+    pistol_thrs0.50A_clust03-000001_AA.pdb 18.1711015479 1237
+    # of atoms used: 1237
+    csv was created!  rmsds.csv
+
+rmsd_calc_dir.py
+-------------------------------------------------------------------------------
+
+The program calculates all-atom rmsds (root-mean-square deviation) for all PDB structures in a given folder and save them to a file, as a matrix (which can be read by <https://github.com/mmagnus/rnastruc_clanstix>).
+
+Usage:
+
+    $ ./rna_calc_rmsd_all_vs_all.py -i test_data -o test_output/matrix.txt
+    calc_rmsd_dir
+    --------------------------------------------------------------------------------
+     # of models: 4
+    ... 1 test_data/struc1.pdb
+    ... 2 test_data/struc2.pdb
+    ... 3 test_data/struc3.pdb
+    ... 4 test_data/struc4.pdb
+    # test_data/struc1.pdb test_data/struc2.pdb test_data/struc3.pdb test_data/struc4.pdb
+    0.0 11.803 4.879 3.982
+    11.803 0.0 12.153 12.043
+    4.879 12.153 0.0 3.487
+    3.982 12.043 3.487 0.0
+    matrix was created!  test_output/matrix.txt
+
+Output:
+
+    $ head test_output/matrix.txt
+    # test_data/struc1.pdb test_data/struc2.pdb test_data/struc3.pdb test_data/struc4.pdb
+    0.0 11.803 4.879 3.982
+    11.803 0.0 12.153 12.043
+    4.879 12.153 0.0 3.487
+    3.982 12.043 3.487 0.0
+
+The program is using Biopython:
+
+Cock, P.J.A. et al. Biopython: freely available Python tools for computational molecular biology and bioinformatics. Bioinformatics 2009 Jun 1; 25(11) 1422-3 http://dx.doi.org/10.1093/bioinformatics/btp163 pmid:19304878
+
--- rna_pdb_tools/utils/rna_calc_rmsd/rna_calc_rmsd.py
+++ rna_pdb_tools/utils/rna_calc_rmsd/rna_calc_rmsd.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python
+#-*- coding: utf-8 -*-
 
-"""rmsd_calc_rmsd_to_target
+"""
+➜  rna_calc_rmsd git:(master) ✗ ./rna_calc_rmsd.py
+rmsd_calc_rmsd_to_target
 --------------------------------------------------------------------------------
-Usage: rmsd_calc_to_target.py [<options>] <pdb files (test_data/*)>
+Usage: rna_calc_rmsd.py [<options>] <pdb files (test_data/*)>
 
 Options:
   -h, --help            show this help message and exit
@@ -18,13 +21,13 @@ Options:
                         included
   --model_ignore_selection=MODEL_IGNORE_SELECTION
                         A/10/O2'
+  -m METHOD, --method=METHOD
   -o RMSDS_FN, --rmsds_fn=RMSDS_FN
                         ouput, matrix
   -v, --verbose         verbose
+"""
 
-.. caution:: this version if BioPython free == should be super fast! """
-
-from rna_pdb_tools.utils.rmsd_calc.lib.rmsd.calculate_rmsd import *
+from rna_pdb_tools.utils.rna_calc_rmsd.lib.rmsd.calculate_rmsd import *
 import sys
 from rna_pdb_tools.utils.extra_functions.select_fragment import select_pdb_fragment_pymol_style, select_pdb_fragment
 import optparse
@@ -61,6 +64,44 @@ def sort_nicely( l ):
    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ]
    l.sort( key=alphanum_key )
    return l
+
+def calc_rmsd_pymol(pdb1, pdb2, method):
+    """Calculate rmsd using PyMOL. Two methods are available: align and fit
+
+    See:
+      Align: http://www.pymolwiki.org/index.php/Align
+      Fit:   http://www.pymolwiki.org/index.php/Fit 
+      (https://pymolwiki.org/index.php?title=Fit&redirect=no)
+
+    Align can return a list with 7 items:
+
+    RMSD after refinement
+    Number of aligned atoms after refinement
+    Number of refinement cycles
+    RMSD before refinement
+    Number of aligned atoms before refinement
+    Raw alignment score
+    Number of residues aligned 
+
+    in this version of function, the function returns `RMSD before refinement`.
+
+
+    """
+
+    import __main__
+    __main__.pymol_argv = ['pymol', '-qc']
+    import pymol  # import cmd, finish_launching
+    pymol.finish_launching()
+
+    pymol.cmd.reinitialize()
+    pymol.cmd.delete('all')
+    pymol.cmd.load(pdb1, 's1')
+    pymol.cmd.load(pdb2, 's2')
+    if method == 'align':
+        return (pymol.cmd.align('s1', 's2')[3], pymol.cmd.align('s1',
+                's2')[4])
+    if method == 'fit':
+        return (pymol.cmd.fit('s1', 's2'), 'fit')
 
 def calc_rmsd(a,b, target_selection, target_ignore_selection, model_selection, model_ignore_selection, verbose):
     """
@@ -128,6 +169,11 @@ if __name__ == '__main__':
                          default='',
                          help="A/10/O2\'")
     
+    optparser.add_option('-m',"--method", type="string",
+                         dest="method",
+                         default='',
+                         help="")
+
     optparser.add_option('-o',"--rmsds_fn", type="string",
                          dest="rmsds_fn",
                          default='rmsds.csv',
@@ -146,14 +192,28 @@ if __name__ == '__main__':
     input_files = args[:] # opts.input_dir
     rmsds_fn = opts.rmsds_fn
     target_fn = opts.target_fn
-
+    method = opts.method
+    print ' method:', method
     target_selection = select_pdb_fragment(opts.target_selection)
     model_selection = select_pdb_fragment(opts.model_selection)
     if target_selection:
-        if opts.verbose: print '  target_selection: ', opts.target_selection, target_selection
+        if opts.verbose:
+            print '  target_selection: #', opts.target_selection, target_selection
+            ts = target_selection
+            print ts
+            resides_in_total = 0
+            for i in target_selection:
+                print i, len(ts[i]) # chain string, a list of residues
+                resides_in_total += len(ts[i])
+            print 'in total:', resides_in_total
     if model_selection:
-        if opts.verbose: print '  model_selection:  ', opts.model_selection, model_selection
-
+        if opts.verbose:
+            print '  model_selection:  ', len(model_selection), opts.model_selection, model_selection
+            resides_in_total = 0
+            for i in model_selection:
+                print i, len(model_selection[i]) # chain string, a list of residues
+                resides_in_total += len(model_selection[i])
+            print 'in total:', resides_in_total
     if opts.target_ignore_selection:
         target_ignore_selection = select_pdb_fragment_pymol_style(opts.target_ignore_selection)
     else:
@@ -179,7 +239,10 @@ if __name__ == '__main__':
 
     c = 1
     for r1 in models:
-        rmsd_curr, atoms = calc_rmsd(r1, target_fn, target_selection, target_ignore_selection, model_selection, model_ignore_selection, opts.verbose)
+        if method == 'align' or method == 'fit':
+            rmsd_curr, atoms = calc_rmsd_pymol(r1, target_fn, method)
+        else:
+            rmsd_curr, atoms = calc_rmsd(r1, target_fn, target_selection, target_ignore_selection, model_selection, model_ignore_selection, opts.verbose)
         r1_basename = os.path.basename(r1)
         print r1_basename, rmsd_curr, atoms
         t += r1_basename + ',' + str(round(rmsd_curr,3)) + ' '
