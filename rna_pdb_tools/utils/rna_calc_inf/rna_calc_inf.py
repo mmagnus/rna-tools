cbbc130ce1880b8619a0edc9e94335df2f0e2d1d
magnus <magnus@genesilico.pl>
2016-12-03 21:57:29 +0100

    rna_calc_inf: testing multiprocessing

--- rna_pdb_tools/utils/rna_calc_inf/rna_calc_inf.py
+++ rna_pdb_tools/utils/rna_calc_inf/rna_calc_inf.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 """
 A tool to calc inf_all, inf_stack, inf_WC, inf_nWC, SNS_WC, PPV_WC, SNS_nWC, PPV_nWC between two structures.
@@ -7,15 +7,19 @@ ClaRNA_play required!
 https://gitlab.genesilico.pl/RNA/ClaRNA_play (internal GS gitlab server)
 
 """
-
+import progressbar
 import argparse
 import sys
 import os
 import subprocess
 import re
 import tempfile
+from multiprocessing import Pool, Lock, Value, Process
+number_processes = 8
 
 from rna_pdb_tools.utils.clarna_app import clarna_app
+
+#counter = Value(c_int)
 
 def get_parser():
     parser =  argparse.ArgumentParser()#usage="%prog [<options>] <pdb files (test_data/*)>")
@@ -44,6 +48,13 @@ def get_parser():
 
     return parser
 
+def do_job(i):
+    i_cl_fn = clarna_app.clarna_run(i, args.force)
+    output = clarna_app.clarna_compare(target_cl_fn,i_cl_fn)
+    c = input_files.index(i)
+    bar.update(100* (c/float(len(input_files))))
+    return output
+
 if __name__ == '__main__':
     print 'rna_calc_inf'
     print '-' * 80
@@ -64,15 +75,27 @@ if __name__ == '__main__':
     else:
         target_cl_fn = clarna_app.clarna_run(target_fn, args.force)    
     out_fn = args.out_fn
+
     # output
     print 'target, fn, inf_all, inf_stack, inf_WC, inf_nWC, SNS_WC, PPV_WC, SNS_nWC, PPV_nWC'
     f = open(out_fn, 'w')
     #t = 'target:' + os.path.basename(target_fn) + ' , rmsd_all\n'
     t = 'target,fn,inf_all, inf_stack, inf_WC, inf_nWC, SNS_WC, PPV_WC, SNS_nWC, PPV_nWC\n'
     f.write(t)
-    for i in input_files:
-        i_cl_fn = clarna_app.clarna_run(i, args.force)
-        output = clarna_app.clarna_compare(target_cl_fn,i_cl_fn)
-        print output
-        f.write(re.sub('\s+', ',', output) + '\n')
-    print 'csv was created! ', out_fn
+
+    bar = progressbar.ProgressBar()
+    p = Pool(number_processes)
+    # print do_job(input_files[0])
+    p.map(do_job, input_files)
+
+    #procs = []
+    #for c, i in enumerate(input_files):#, range(len(input_files))):
+    #    proc = Process(target=do_job, args=(i,))
+    #    procs.append(proc)
+    #    proc.start()
+
+    #    print i
+    #    global methods, c
+    #    f.write(re.sub('\s+', ',', output) + '\n')
+    #    bar.update(c/float(len(input_files)))
+    #print 'csv was created! ', out_fn
