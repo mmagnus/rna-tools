cb78979f7d4bdf5bd1cca6aa3d9080ef36ba5f29
magnus <magnus@genesilico.pl>
2017-02-15 02:03:54 +0100

    rosetta: run add -c (--cpus)

--- rna_pdb_tools/utils/rna_rosetta/rna_rosetta_run.py
+++ rna_pdb_tools/utils/rna_rosetta/rna_rosetta_run.py
@@ -7,6 +7,8 @@ http://www.sciencedirect.com/science/article/pii/S0076687914000524
 
 The script makes (1) a folder for you job, with seq.fa, ss.fa, input file is copied as input.fa to the folder (2) make helices (3) prepare rosetta input files (4) sends jobs to the cluster.
 
+Mind that headers will be trimmed to 5 characters (``header[:5]``). The header is take from the fast file (``>/header/``) not from the filename of your Fasta file.
+
 If one of the helices is missing you will get::
 
     IOError: [Errno 2] No such file or directory: 'helix1.out'
@@ -38,6 +40,7 @@ import os
 import glob
 import subprocess
 import shutil
+import math
 
 ROOT_DIR_MODELING = '/home/magnus/'
 
@@ -51,6 +54,7 @@ def get_parser():
                         action='store_true')
     parser.add_argument('-g', '--go', help= go.__doc__,
                         action='store_true')
+    parser.add_argument('-c', '--cpus', help='default: 200', default=200)
 
     parser.add_argument('file', help= textwrap.dedent("""file:\n>a04\nUAUAACAUAUAAUUUUGACAAUAUGGGUCAUAAGUUUCUACCGGAAUACCGUAAAUAUUCUGACUAUGUAUA\n((((.((((...((.((((.......)))).))........(.(((((.......))))).)..))))))))"""))
     return parser
@@ -75,9 +79,11 @@ def prepare_folder(args,header,seq,ss,path):
     shutil.copyfile(args.file, d + 'input.fa')
 
 def prepare_helices():
-    """Make helices (wrapper around 'helix_preassemble_setup.py')"""
+    """Make helices (wrapper around 'helix_preassemble_setup.py')
+
+    .. warning:: I think multiprocessing of helixX.run does not work."""
     cmd = 'helix_preassemble_setup.py -secstruct ss.fa -fasta seq.fa'
-    os.system(cmd)
+    #os.system(cmd)
     #
     helix_runs = glob.glob('*RUN')
     print helix_runs
@@ -87,29 +93,40 @@ def prepare_helices():
         f.write(open(h).read().strip() + ' & \n')
     f.close()
     
+    ## does not work (!) 
+    #os.system('chmod +x CMDLINES')
+    #os.system('./CMDLINES')
+    ## ./CMDLINES: 2: source: not found
+    
+    ## runs multiprocessing, but does not wait for the rest of the program
+    ## hmm... it does not wait because I used & ???
+    #os.system('bash HRUNS') 
+    
     p = subprocess.Popen(open('HRUNS').read(), shell=True, stderr=subprocess.PIPE)
     p.wait()
     stderr = p.stderr.read().strip()
     if stderr:
         print stderr
 
-def prepare_rosetta(header):
+def prepare_rosetta(header, cpus):
     """Repare ROSETTA using rna_denovo_setup.py
 
-    change to 50 per job (!) change manually right now
-    50 * 100 = 10k ?
-    40 * 500 = 20k
-    """
+    cpus is used to calc nstruc per job to get 20k structures per full run::
+
+      nstruct = int(math.floor(20000/cpus))
+      e.g. 
+      40 (nstruc) = 20k / 500 (cpus) """
     # get list line
     helices = open('CMDLINES').readlines()[-1].replace('#','')
-    nstruct = 40 #500
-    njobs = 500
+    njobs = cpus # 500
+    nstruct = int(math.floor(20000/cpus)) # 20000/500 -> 40
+
     cmd = 'rna_denovo_setup.py -fasta seq.fa -secstruct_file ss.fa -cycles 20000 -no_minimize -nstruct ' + str(nstruct) + ' ' + helices
     print cmd
     os.system(cmd)
     # change to 50 per job (!)
     # 50 * 100 = 10k ?
-    cmd = 'rosetta_submit.py README_FARFAR o ' + str(njobs) + ' 100 ' + header[:3]
+    cmd = 'rosetta_submit.py README_FARFAR o ' + str(njobs) + ' 100 ' + header[:5]
     print cmd
     os.system(cmd)    
 
@@ -127,7 +144,7 @@ def main():
         header = f.readline().strip().replace('>','')
         seq = f.readline().strip()
         ss = f.readline().strip()
-
+        cpus = int(args.cpus)
         print 'run rosetta for:'
         print header
         print seq
@@ -145,7 +162,7 @@ def main():
         if args.helices:
             prepare_helices()
         if args.rosetta:
-            prepare_rosetta(header)
+            prepare_rosetta(header, cpus)
         if args.go:
             go()
 
