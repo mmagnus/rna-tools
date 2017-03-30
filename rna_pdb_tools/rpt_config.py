a5271733968de665b80bba8b30411f6041f80b7d
Pietro Boccaletto <pboccaletto@genesilico.pl>
2017-03-30 16:59:37 +0200

    Bugfix, missing VARNA path

--- rna_pdb_tools/rpt_config.py
+++ rna_pdb_tools/rpt_config.py
@@ -1,9 +1,11 @@
 SIMRNA_DATA_PATH = None
 RCHIE_PATH = None
 QRNAS_PATH = None
+VARNA_PATH = None
 RFAM_DB_PATH = None # path to Rfam.cm
 
 try:
-    from rpt_config_local import *
+    from rna_pdb_tools.rpt_config_local import *
 except ImportError:
     pass
+
