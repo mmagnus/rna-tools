8412b9ffff9602fe2a5dfd538ff5d99856eba602
magnus <magnus@genesilico.pl>
2017-04-04 12:19:08 +0200

    config: fix importing issue (?)

--- rna_pdb_tools/rpt_config.py
+++ rna_pdb_tools/rpt_config.py
@@ -5,7 +5,7 @@ VARNA_PATH = None
 RFAM_DB_PATH = None # path to Rfam.cm
 
 try:
-    from rna_pdb_tools.rpt_config_local import *
+    from rpt_config_local import *
 except ImportError:
     pass
 
