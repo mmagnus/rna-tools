e17e8326cd85871067285c62870c19cca8a76c48
mmagnus <m.magnus@zoho.com>
2017-07-12 18:31:15 +0200

    config: fix importing

--- rna_pdb_tools/rpt_config.py
+++ rna_pdb_tools/rpt_config.py
@@ -6,8 +6,19 @@ RFAM_DB_PATH = None # path to Rfam.cm
 CONTEXTFOLD_PATH = None
 CPUS_CLUSTER = 1000
 DIFF_TOOL="diff"
-RNA_ROSETTA_RUN_ROOT_DIR_MODELING = None
-QRNAS_PATH = ''
+
+RNA_ROSETTA_RUN_ROOT_DIR_MODELING = "/home/magnus/rosetta-runs"
+EASY_CAT_PATH=""
+
+import os
+try:
+    PATH = os.environ['RNA_PDB_TOOLS']
+except KeyError:
+    print ('Set up RNA_PDB_TOOLS, see Installation note')
+    pass
+else:
+    QRNAS_PATH = os.getenv('QRNAS_PATH', PATH + '/opt/qrnas/')
+
 try:
     from rpt_config_local import *
 except ImportError:
