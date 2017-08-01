72e0036119c5738ccd77b318ee3ddc3186ed6747
magnus <magnus@genesilico.pl>
2017-08-01 23:49:44 +0200

    rna_rosetta: add test

---
+++ rna_pdb_tools/utils/rna_rosetta/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import subprocess
+import sys
+
+
+def test():
+    cmd = "./rna_rosetta_run.py -r -g -n 10 -c 1 --sandbox test_data test_data/tetraloop.fa"
+    # - i - e
+    o = subprocess.Popen(cmd, shell=True, stdout=sys.stdout.flush(), stderr=subprocess.PIPE)
+    stderr = o.stderr.read().strip()
+    assert stderr == ''
+
+
+if __name__ == '__main__':
+    test()
