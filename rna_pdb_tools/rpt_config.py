b3c0d3927d158ea8396d64c0e0f365510f352594
magnus <magnus@genesilico.pl>
2017-08-02 16:02:26 +0200

    auto-pep8

--- rna_pdb_tools/rpt_config.py
+++ rna_pdb_tools/rpt_config.py
@@ -2,10 +2,10 @@ SIMRNA_DATA_PATH = None
 RCHIE_PATH = None
 QRNAS_PATH = None
 VARNA_PATH = None
-RFAM_DB_PATH = None # path to Rfam.cm
+RFAM_DB_PATH = None  # path to Rfam.cm
 CONTEXTFOLD_PATH = None
 CPUS_CLUSTER = 1000
-DIFF_TOOL="diff"
+DIFF_TOOL = "diff"
 
 RNA_ROSETTA_RUN_ROOT_DIR_MODELING = "/home/magnus/rosetta-runs"
 RNA_ROSETTA_NSTRUC = 10000
@@ -25,4 +25,3 @@ try:
     from rpt_config_local import *
 except ImportError:
     pass
-
