f377b9137e8e0df57fabdf21d116cc1cc0f09a2a
magnus <magnus@genesilico.pl>
2017-08-03 19:13:08 +0200

    rna_rosetta_n.py: add --verbose to get a filename of a slient file

--- rna_pdb_tools/utils/rna_rosetta/rna_rosetta_n.py
+++ rna_pdb_tools/utils/rna_rosetta/rna_rosetta_n.py
@@ -1,36 +1,44 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""
+"""rna_roseta_n.py - show me # of structure in a silent file
+
 Example::
 
-  $ rna_rosetta_n.py ade.out
-  21594
+     $ rna_rosetta_n.py ade.out
+     21594
 
 """
-
+from __future__ import print_function
 import subprocess
 import argparse
 
+
 def get_parser():
-    parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-v', '--verbose', action='store_true')
     parser.add_argument('file', help='ade.out')
     return parser
 
+
 def get_no_structures(file):
-    p = subprocess.Popen('cat ' + file + ' | grep SCORE | wc -l', shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    p.wait()
+    p = subprocess.Popen('cat ' + file + ' | grep SCORE | wc -l', shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stderr = p.stderr.read().strip()
     if stderr:
         print(stderr)
     return int(p.stdout.read().strip()) - 1
 
+
 def run():
-    """Pipline for modeling RNA"""
+    """Pipline for modeling RNA."""
     args = get_parser().parse_args()
     ns = get_no_structures(args.file)
+    if args.verbose:
+        print(args.file.ljust(30), end='')
     print(ns)
 
-#main
+
+# main
 if __name__ == '__main__':
     run()
-    
