23cf91d2289dc65f6d70880e0fbc18bab0b04253
magnus <magnus@genesilico.pl>
2017-08-03 19:11:55 +0200

    shell-aliases - shortcuts in your shell

---
+++ rna_pdb_tools/shell-aliases.sh
@@ -0,0 +1,8 @@
+#
+#  shell-aliases
+#  install in .bashrc/.zshrc
+#
+#  source "/home/magnus/src/rna-pdb-tools/rna_pdb_tools/shell-aliases.sh"
+#
+
+alias rna_rosetta_n_loop="find . -maxdepth 2 -iname '*out' -type f -not -name 'helix*' -exec rna_rosetta_n.py --verbose {} \;"
