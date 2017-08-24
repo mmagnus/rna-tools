d298f6af986ca7b3e2ffedfd7b18f9635f510adf
mmagnus <m.magnus@zoho.com>
2017-08-24 23:02:37 +0200

    logging: add function name

--- rna_pdb_tools/rpt_logging.py
+++ rna_pdb_tools/rpt_logging.py
@@ -3,7 +3,7 @@ logger = logging.getLogger()
 handler = logging.StreamHandler()
 logger.addHandler(handler)
 formatter = logging.Formatter(
-    '%(asctime)-15s %(filename)s %(message)s')
+    '%(asctime)-15s %(filename)s::%(funcName)s::%(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
