5c498a7ad0825d8e693bf32b2cd2e25dda8328f1
mmagnus <m.magnus@zoho.com>
2017-09-01 01:06:48 +0200

    rna_seq_get_bps.py - now it also work with pseudoknots.

--- rna_pdb_tools/utils/rna_filter/rna_seq_get_bps.py
+++ rna_pdb_tools/utils/rna_filter/rna_seq_get_bps.py
@@ -14,6 +14,8 @@ Usage::
 
     $ rna_seq_get_bps.py ade_ss.fa --offset 12
     [[13, 83], [14, 82], [15, 81], [16, 80], [17, 79], [18, 78], [19, 77], [20, 76], [21, 75], [25, 45], [26, 44], [27, 43], [28, 42], [29, 41], [30, 40], [54, 72], [55, 71], [56, 70], [57, 69], [58, 68], [59, 67]]
+
+Now it also work with pseudoknots.
 """
 from __future__ import print_function
 
@@ -37,11 +39,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     id, seq, ss = open(args.file).read().strip().split('\n')
-    pairs = parse_vienna_to_pairs(ss)[0]
+    pairs, pairs_pk = parse_vienna_to_pairs(ss)
 
     if args.offset:
         npairs = []
         for pair in pairs:
             npairs.append([pair[0] + args.offset, pair[1] + args.offset])
-        pairs = npairs
-    print(pairs)
+        for pair in pairs_pk:
+            npairs.append([pair[0] + args.offset, pair[1] + args.offset])
+    else:
+        npairs = pairs + pairs_pk
+    npairs.sort()
+    print(npairs)
