1cfc263dc2e885ed298bc01a224cb0cdfa80ff7c
mmagnus <m.magnus@zoho.com>
2017-09-02 19:05:30 +0200

    rna_filter.py: fixed doctest

--- rna_pdb_tools/utils/rna_filter/rna_filter.py
+++ rna_pdb_tools/utils/rna_filter/rna_filter.py
@@ -8,17 +8,11 @@ The format of restraints::
 
 Usage::
 
-    $ python rna_filter.py -r restraints.txt -s test_data/CG.pdb -v
-    (d:A1-A2 <  10.0  1)|(d:A2-A1 <= 10 1)
-     restraints [('A1', 'A2', '<', '10.0', '1'), ('A2', 'A1', '<=', '10', '1')]
+    $ python rna_filter.py -r test_data/restraints.txt -s test_data/CG.pdb
+     d:A1-A2 10.0 measured: 6.58677550096 [x]
+    test_data/CG.pdb 1.0 1 out of 1
 
-    test_data/CG.pdb
-     mb for  A1 [ 54.729   28.9375  41.421 ]
-     mb for  A2 [ 55.3425  35.3605  42.7455]
-      d:A1-A2 6.58677550096
-      d:A2-A1 6.58677550096
-
-    $ python rna_filter.py -r restraints.txt -t test_data/CG.trafl -v
+    # $ python rna_filter.py -r test_data/restraints.txt -t test_data/CG.trafl
     (d:A1-A2 <  10.0  1)|(d:A2-A1 <= 10 1)
      restraints [('A1', 'A2', '<', '10.0', '1'), ('A2', 'A1', '<=', '10', '1')]
 
@@ -29,8 +23,9 @@ Usage::
       mb for A2 [ 55.3425  35.3605  42.7455]
       mb for A1 [ 54.729   28.9375  41.421 ]
        d:A2-A1 6.58677550096
-
+    # this ^ is off right now
 """
+
 from __future__ import print_function
 import logging
 from rna_pdb_tools.rpt_logging import logger
