246bd97907c11111a874102192930c286c44f0e8
mmagnus <m.magnus@zoho.com>
2017-09-02 15:34:43 +0200

    rna_filter: proof of concept with doctest_cmds

--- rna_pdb_tools/utils/rna_filter/test_rna_filter.py
+++ rna_pdb_tools/utils/rna_filter/test_rna_filter.py
@@ -2,9 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import print_function
-import argparse
 import subprocess
-import git
+import doctest_cmds
 
 def run_cmd(cmd, verbose=True):
     if verbose:
@@ -15,6 +14,7 @@ def run_cmd(cmd, verbose=True):
     err = o.stderr.read().strip().decode()
     return out, err
 
+
 def is_modifed(fn):
     out, err = run_cmd('git status %s' % fn)
     if 'modified' in out or 'untracked file' in out:
@@ -22,8 +22,15 @@ def is_modifed(fn):
     else:
         return False
 
+
 def test_rna_filter():
     fn = "test_data/rna_filter.txt"
-    cmd = "python rna_filter.py -r test_data/restraints.txt -s test_data/CG.pdb | tee test_data/rna_filter.txt"
+    cmd = "python rna_filter.py -r test_data/restraints.txt -s test_data/CG.pdb | " \
+          "tee test_data/rna_filter.txt"
     out, err = run_cmd(cmd)
-    assert is_modifed(fn) == False
+    assert is_modifed(fn) is False
+
+
+
+def test_rna_filter_cmd():
+    assert doctest_cmds.is_ok('rna_filter.py') is True
