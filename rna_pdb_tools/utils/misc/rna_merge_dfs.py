712ae3f80ae0e14f968c21a5a6b936892288a5d7
mmagnus <m.magnus@zoho.com>
2017-09-06 23:35:12 +0200

    misc: some scripts for mq backend

---
+++ rna_pdb_tools/utils/misc/rna_merge_dfs.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import print_function
+import pandas as pd
+import argparse
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('csv1', help="", default="")
+    parser.add_argument('--sep1', help="default is ,; can be also '\t'", default=",")
+    parser.add_argument('csv2', help="", default="")
+    parser.add_argument('--sep2', help="default is ,; can be also '\t'", default=",")
+    parser.add_argument('mergeon', help="merge on column", default="")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    parser.add_argument('--output', help="output csv", default="merged.csv")
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    df1 = pd.read_csv(args.csv1, delimiter=args.sep1)
+    df2 = pd.read_csv(args.csv2, delimiter=args.sep2)
+    print(df1)
+    print(df2)
+
+    merged = pd.merge(df1, df2, on=args.mergeon)
+    print(merged)
+    merged.to_csv(args.output)
+    print('Saved to %s' % args.output)
---
+++ rna_pdb_tools/utils/misc/rna_mq_exp.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import print_function
+import pandas as pd
+import argparse
+import os
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('csv', help="", default="")
+    parser.add_argument('--sep', help="default is ,; can be also '\t'", default=",")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    parser.add_argument('--output', help="output csv", default="merged.csv")
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    for l in open(args.csv):
+        if l.startswith('fn'):
+            pass
+        else:
+            fn = l.split(',')[0].strip()
+            mq = float(l.split(',')[1].strip())
+            exp = float(l.split(',')[2].strip())
+            mq_exp = float(mq) +(1 * ((1 - exp) * mq))
+            print(','.join([fn, str(mq), str(exp), str(mq_exp)]))
