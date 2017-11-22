a3e62eeccbfed59f45e45e405d85d0a063c0e8dc
mmagnus <m.magnus@zoho.com>
2017-11-22 19:41:30 +0100

    rna_filter: update dca script

---
+++ rna_pdb_tools/utils/rna_filter/rna_dca_mapping.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+"""rna_dca_mapping.py
+
+This function is divided into three parts (and panels in the output):
+
+1. map interactions on the gapped sequence,
+2. map interactions on the sequence without gaps,
+3. map interactions on the sequence that might have insertions (insertions are marked with lower-case letters).
+
+Gseq and Seq are files in the Fasta format with a header, sequence, and secondary structure. Warning: the script is not tested (yet) with secondary structure with pseudoknots. It should work as well, but it is not tested yet. Secondary structures should be defined in the Vienna  ("dot-bracket") notation.
+
+`--noshort` will remove interactions that the distance between them is less than 6. For example, an interaction like [1,3] will be removed.
+
+`--noss` will remove all DCA interactions that come because of secondary structure::
+
+   (..)
+   [1,4] DCA interaction will be removed.
+
+Q: your interactions start from 0 or 1?
+
+For a DCA interaction file, ' ' is used for separator at the moment.
+
+Required:  rna_pdb_tools.SecondaryStructure to parse secondary structures in the Vienna format (dot-bracket notation).
+"""
+
+import pandas as pd
+import sys
+import numpy as np
+import argparse
+
+from rna_pdb_tools.SecondaryStructure import parse_vienna_to_pairs
+
+def rna_dca_mapping(seqfn, gseqfn, file_interactions, noss, noshort, offset, verbose):
+    """This function is deviede into
+
+    .. warning:: in the line that we load parameters, watch for sep argument that defines seperator of your file (line21)
+    """
+    v = verbose
+
+    #
+    # Load the data, open files and parse information
+    #
+    # final ss and seq, ungapped
+    f = open(seqfn)
+    header = f.readline().strip()  # get rid of header
+    seq = f.readline().strip()
+    ss = f.readline().strip()
+    # gapped
+    f = open(gseqfn)
+    header = f.readline().strip()  # get rid of header
+    gseq = f.readline().strip()
+    gss = f.readline().strip()
+    # DCA
+    df = pd.read_csv(file_interactions,sep=" ")
+    interactions = zip(df['i'].tolist(), df['j'].tolist())
+    #
+    # Show input
+    #
+    # [(38, 51), (7, 110), (37, 52) from the input
+    interactions.sort()
+    print 'interactions:\n', interactions
+    #
+    # Process unmapped scores on gaped sequence
+    # I panel
+    #
+    pairs = parse_vienna_to_pairs(gss)[0]
+    print 'pairs', pairs
+    print 'UNMAPPED SCORES ' + '/' * len(gseq)
+    print '123456789112345678921234567893123456789412345678951234567896123456789712345678981234567899123456789'
+    print gseq
+    print gss
+
+    for i in interactions:
+        # form 0 or from 1 ?! ## be careful here! scores starts from 0 or 1 !?
+        ij = [0,0]
+        ij[0] = i[0] + 1
+        ij[1] = i[1] + 1
+        i = ij
+
+        if noshort:
+            delta = i[1] - i[0]
+            if delta < 6:
+                continue
+
+        # remove interactions from gaps
+        if gseq[i[0] - 1] == '-' or gseq[i[1] - 1] == '-':
+            continue
+
+        if noss:
+            if i in pairs:
+                continue
+
+        line_new = 'x'.rjust(i[0]) + 'x'.rjust(i[1] - i[0]) + str(i).rjust(len(gseq) - i[1] + 10)
+        print line_new
+    #
+    # How this mapping works, kurwa?
+    # II panel
+    #
+    print 'MAPPED SCORES //' + '/' * len(gseq)
+    mapped_interactions = []
+    for i in interactions:
+        ij = i
+        #ij = [0,0]
+        #ij[0] = i[0] - 1
+        #ij[1] = i[1] - 1
+        if v: print 'ij:', ij
+        if v: print gseq[ij[0]], gseq[ij[1]]
+
+        # ok, here I test if this pair comes from gaps, . -> -
+        if gseq[ij[0]] == '-' or gseq[ij[1]] == '-':
+            if v: print 'Removed Interaction:', ij
+        else:
+            [a,b]=[ij[0] - gseq[:ij[0]].count('-') +1, ij[1] - gseq[:ij[1]].count('-') + 1,]
+            if v: print i, '->', a, b
+            mapped_interactions.append([a, b])
+
+    print 'Mapped Interactions:\n', mapped_interactions
+    if v:
+        for i in mapped_interactions:
+            print str(i)
+
+    pairs = parse_vienna_to_pairs(gss)[0]
+    print 'pairs', pairs
+    print '123456789112345678921234567893123456789412345678951234567896123456789712345678981234567899123456789'
+    print gseq.replace('-','')  # what is the gap character, - or . ?
+    print gss.replace('-', '')
+    mapped_interactions.sort()
+
+    filtered_interactions = []
+    pairs = parse_vienna_to_pairs(gss.replace('-', ''))[0]
+    for i in mapped_interactions:
+        if noshort:
+            delta = i[1] - i[0]
+            if delta < 6:
+                continue
+
+        # remove if there is a gap
+        if seq[i[0] - 1] == '-' or seq[i[1] - 1] == '-':
+            continue
+
+        if noss:
+            if i in pairs:
+                continue
+
+        line_new = 'x'.rjust(i[0]) + 'x'.rjust(i[1] - i[0]) + str(i).rjust(len(seq) - i[1] + 10)
+        print line_new
+    #
+    # How to include a gap in the mapping?
+    # III panel, the final
+    #
+    print seq
+    print ss
+
+    pairs = parse_vienna_to_pairs(ss.replace('-', ''))[0]
+
+    def n_lower_chars(string):
+        """ https://stackoverflow.com/questions/10953189/count-lower-case-characters-in-a-string """
+        return sum(1 for c in string if c.islower())
+
+    print 'FINAL MAPPING //' + '/' * len(seq)
+    nmapped_interactions = []
+    for i in mapped_interactions:
+        ij = i
+        if v: print 'ij:', ij
+        if v: print seq[ij[0] - 1], seq[ij[1] - 1]
+        # gap mapping
+        a = ij[0] + n_lower_chars(seq[:ij[0]])
+        b = ij[1] + n_lower_chars(seq[:ij[1]])
+        if v: print i, '->', a,b
+        nmapped_interactions.append([a,b])
+    mapped_interactions = nmapped_interactions
+
+    print(seq)
+    print(ss)
+    pairs = parse_vienna_to_pairs(ss)[0]
+    nmapped_interactions = []
+    for i in mapped_interactions:
+        if noshort:
+            delta = i[1] - i[0]
+            if delta < 6:
+                continue
+
+        if seq[i[0] - 1] == '-' or seq[i[1] - 1] == '-':
+            continue
+
+        if noss:
+            if i in pairs:
+                continue
+
+        line_new = 'x'.rjust(i[0]) + 'x'.rjust(i[1] - i[0]) + str(i).rjust(len(seq) - i[1] + 10)
+        nmapped_interactions.append([i[0], i[1]])
+        print line_new
+    mapped_interactions = nmapped_interactions
+
+    if offset:
+        # e.g. filter_interaction is [[18, 71]], if offset is 10 then it will give you [[28, 81]]
+        nmapped_interactions = [[x[0] + offset, x[1] + offset] for x in mapped_interactions]
+    mapped_interactions = nmapped_interactions
+
+    print mapped_interactions
+    print 'draw_dists(' + str(mapped_interactions) + ')'
+    print 'output file:', file_interactions+"_mapped.csv"
+    a = pd.DataFrame(list(mapped_interactions), columns=["i","j"])
+    a.to_csv(file_interactions+"_mapped.csv",sep=" ")
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(description=__doc__ , formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('--seq', help='seq fn in Fasta format')
+    parser.add_argument('--gseq', help='gapped sequence and secondary structure (like in the alignment used for DCA) in Fasta format')
+    parser.add_argument('--dca', help='file with parsed interactions')
+    parser.add_argument('--offset', help="offset", type=int)
+    parser.add_argument('--noss', help='filter out ss from plot', action='store_true')
+    parser.add_argument('--verbose', help='be verbose', action='store_true')
+    parser.add_argument('--noshort', help='filter out short interactions, dist in seq < 6 nt', action='store_true')
+    return parser
+
+if __name__=="__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+    rna_dca_mapping(args.seq, args.gseq, args.dca, args.noss, args.noshort, args.offset, args.verbose)
