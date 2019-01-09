54d8d7715fb8b0d06e30cf503be84757d1088494
Marcin Magnus <mag_dex@o2.pl>
2019-01-09 11:37:10 +0100

    clanstix with smart group name picking done (fancy ways!) #72

--- rna_pdb_tools/utils/clanstix/test.sh
+++ rna_pdb_tools/utils/clanstix/test.sh
@@ -3,3 +3,7 @@ echo ------------------------------------------------------------
 echo input test_data/matrix.txt
 python rna_clanstix.py test_data/matrix.txt clans_run.txt
 echo clans_run.txt generated
+
+python rna_clanstix.py --groups-auto 10 --color-by-homolog --shape-by-source test_data/gmp_ref_mapping_pk_refX.txt test_data/gmp_color_by_homolog_shape_by_source.clans
+python rna_clanstix.py --groups-auto 10 --color-by-homolog test_data/gmp_ref_mapping_pk_refX.txt test_data/gmp_color_by_homolog.clans
+python rna_clanstix.py test_data/gmp_ref_mapping_pk_refX.txt test_data/gmp.clans
