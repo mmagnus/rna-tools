39af33af939805f5703e54c33725cd0baf73d174
Marcin Magnus <mag_dex@o2.pl>
2019-04-21 12:14:15 +0200

    Clean up configs, logging after the rename

--- rna_tools/rpt_logging.py
+++
@@ -1,9 +0,0 @@
-import logging
-logger = logging.getLogger()
-handler = logging.StreamHandler()
-logger.addHandler(handler)
-formatter = logging.Formatter(
-    '%(asctime)-15s %(filename)s::%(funcName)s::%(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-logger.setLevel(logging.INFO)
