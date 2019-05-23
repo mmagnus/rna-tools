d0151aa09044e25c251053d923eac8fc30690c2d
Marcin Magnus <mag_dex@o2.pl>
2019-05-23 23:33:36 +0200

    rna_align_find_seq_in_alignment.py: add to install, fix, fasta is fasta right now

--- rna_tools/tools/rna_alignment/rna_align_find_seq_in_alignment.py
+++ rna_tools/tools/rna_alignment/rna_align_find_seq_in_alignment.py
@@ -2,45 +2,47 @@
 
 """Usage::
 
-    $ python rna_align_find_seq_in_alignment.py -a test_data/RF00167.stockholm.sto -f test_data/xx.fa
-    Match: CP000903.1/3101736-3101822
-    ID: CP000903.1/3101736-3101822
-    Name: CP000903.1
-    Description: CP000903.1/3101736-3101822
+    $ rna_align_find_seq_in_alignment.py -a pistol_noPk.sto -s hcf.fa
+    ('Match:', 'HCF12C_58327/229-301')
+    ID: HCF12C_58327/229-301
+    Name: HCF12C_58327
+    Description: HCF12C_58327/229-301
     Number of features: 0
-    /start=3101736
-    /end=3101822
-    /accession=CP000903.1
-    Seq('CAC-U-CGUAUAUACUCGGUAAUAUGG-UCCGAGC-GUUUCUACCUAGUUCCCA...UU-', SingleLetterAlphabet())
+    /start=229
+    /end=301
+    /accession=HCF12C_58327
+    Per letter annotation for: secondary_structure
+    Seq('UGCCGUUUG-AGCGGCA-UUAAACA--GGUCU-UAAGCUCAA-AGCG-UCACCG...ACA', SingleLetterAlphabet())
 
 In input file and input alignment all `-` are removed.
 
 Or use -v::
 
-    $ rna_align_find_seq_in_alignment.py -f 4lvv.seq -a RF01831.stockholm.sto -v
+    $ rna_align_find_seq_in_alignment.py -s 4lvv.seq -a RF01831.stockholm.sto -v
     UCAGAGUAGAAAACGAUGCGUUAAGUGUCCAGCAGACGGGGAGUUGCUGCCGGAACGAAAAGCAAAGCUUGCGGUAUCGUUUUCGCAUCCCGCUGA
     GCAGAGUAGACACAUGUGCGUUAAGUGCCGGAUGAACAGGGAGUUGUCACCCGGACGAAAAGAAAAUCUUGCGGUACAUGAGUCGCAUCCCGCUGC
     GCAGAGUAGGUUUGUGUGCGUUAAGUGCUGGUUGAACAGGGAGUUGUCAGCCGGACGAAAAGAUUUUCUUGCGGUACACGAAUCGCAUCCCGCUGC
 
 so you can combine this with `grep`::
 
-   $ rna_align_find_seq_in_alignment.py -f 4lvv.seq -a RF01831.stockholm.sto -v | grep UGUUGGGGUAGGAAUUACCGAGUUAUUGUCCAGCGGAACGGAAUGUGAACGCU
+   $ rna_align_find_seq_in_alignment.py -s 4lvv.seq -a RF01831.stockholm.sto -v | grep UGUUGGGGUAGGAAUUACCGAGUUAUUGUCCAGCGGAACGGAAUGUGAACGCU
    UGUUGGGGUAGGAAUUACCGAGUUAUUGUCCAGCGGAACGGAAUGUGAACGCUGGAAGGAAGUAUUAGCUGCUUGAUAAUUUCGCAUUCACCACA
 
 .. warning:: requires http://biopython.org/DIST/docs/api/Bio.AlignIO.StockholmIO-module.html
 
 """
 
-from rna_tools.utils.rna_alignment.rna_alignment import RNAalignment
+from rna_tools.tools.rna_alignment.rna_alignment import RNAalignment
 
 import sys
 import argparse
 from Bio import AlignIO
 
+
 def get_parser():
     parser = argparse.ArgumentParser(description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument('-a', '--alignment', help="alignment in stockholm format", required=True)
-    parser.add_argument('-f', '--file', help="fasta seq", required=True)
+    parser.add_argument('-s', '--seq', help="fasta seq", required=True)
     parser.add_argument('-v', '--verbose', help="", action='store_true', default=False)
     return parser
 
@@ -49,7 +51,9 @@ if __name__ == '__main__':
     args = parser.parse_args()
     #args = parser.parse_args(['-f' , 'test_data/4lvv.seq', '-a', 'test_data/RF01831.stockholm.sto'])
 
-    seq = open(args.file).readline().strip()
+    f = open(args.seq)
+    header = f.readline().strip()
+    seq = f.readline().strip()
 
     a = RNAalignment(args.alignment)
     a.find_seq(seq, args.verbose)
