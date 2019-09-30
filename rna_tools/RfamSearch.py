7352e064fd70dc41e602b6b487087593dc4a3072
Marcin Magnus <mag_dex@o2.pl>
2019-09-30 15:12:28 +0200

    RfamSearch: improve doc

--- rna_tools/RfamSearch.py
+++ rna_tools/RfamSearch.py
@@ -23,14 +23,25 @@ class RfamSearch():
 
     - download the database from ftp://ftp.ebi.ac.uk/pub/databases/Rfam/CURRENT (file: Rfam.cm.gz, ~30mb)
     - install http://eddylab.org/infernal/
-    - set up ``RFAM_DB_PATH`` in the config file of rna-pdb-tools.
+    - set up ``RFAM_DB_PATH`` in the config file of rna-tools.
+    - compress Rfam.cm
+    
+    Example of compressing the database::
+
+         $ cmpress Rfam.cm
+         Working...    done.
+         Pressed and indexed 3016 CMs and p7 HMM filters (3016 names and 3016 accessions).
+         Covariance models and p7 filters pressed into binary file:  Rfam.cm.i1m
+         SSI index for binary covariance model file:                 Rfam.cm.i1i
+         Optimized p7 filter profiles (MSV part)  pressed into:      Rfam.cm.i1f
+         Optimized p7 filter profiles (remainder) pressed into:      Rfam.cm.i1p
 
     Cite: Nawrocki and S. R. Eddy, Infernal 1.1: 100-fold faster RNA homology searches, Bioinformatics 29:2933-2935 (2013). """  # noqa
 
     def __init__(self):
         pass
 
-    def cmscan(self, seq):
+    def cmscan(self, seq, verbose=False):
         """Run cmscan on the seq.
 
         Usage::
@@ -44,6 +55,9 @@ class RfamSearch():
         :param seq: string
         :returns: result
         :rtype: string """
+        #if verbose:
+        print('RFAM_DB_PATH', RFAM_DB_PATH)
+
         # make tmp file
         tf = tempfile.NamedTemporaryFile(delete=False)
         tf.name += '.fa'
@@ -56,6 +70,7 @@ class RfamSearch():
 
         # run cmscan
         cmd = 'cmscan -E 1 ' + RFAM_DB_PATH + ' ' + tf.name + '  > ' + of.name
+        print(cmd)
         o = subprocess.Popen(
             cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         # out = o.stdout.read().strip()
