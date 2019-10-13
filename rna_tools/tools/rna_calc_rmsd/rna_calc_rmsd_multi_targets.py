52f2a031b740597ca505a812df45a6d2f229f122
Marcin Magnus <mag_dex@o2.pl>
2019-10-13 23:04:04 +0200

    rna_calc_rmsd_multi_targets.py: add this tool and re-design data sets

--- rna_tools/tools/rna_calc_rmsd/rna_calc_rmsd_multi_targets.py
+++ rna_tools/tools/rna_calc_rmsd/rna_calc_rmsd_multi_targets.py
@@ -9,6 +9,8 @@
 """
 import argparse
 import os
+import pandas as pd
+
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -19,17 +21,57 @@ def get_parser():
                         action="store_true", help="be verbose")
     parser.add_argument("--models", help="", nargs='+')
     parser.add_argument("--targets", help="", nargs='+')
+    parser.add_argument("--output-csv", help="", default='rna_calc_rmsd_multi_targets_output.csv')
+    parser.add_argument("--model-selection",
+                         default='',
+                         help="selection, e.g. A:10-16+20, where #16 residue is included")
+    parser.add_argument("--target-selection",
+                         default='',
+                         help="selection, e.g. A:10-16+20, where #16 residue is included")
+    
     return parser
+
+
+def hr(t):
+    print
+    if t:
+        print(t)
+    print('-' * 80)
 
 
 if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
     
-    print(args.models)
-    print(args.targets)
+    if args.verbose:
+        print(args.models)
+        print(args.targets)
 
+    rmsds_files = []
+    # run rmsd_calc_rmsd.py
     for target in args.targets:
-        cmd = "rna_calc_rmsd.py -t " + target + " " + ' '.join(args.models)
+        hr(target)
+        outfile = os.path.basename(target).replace('.pdb', '.csv')
+        cmd = "rna_calc_rmsd.py -t " + target + " " + ' '.join(args.models) + ' -o ' + outfile + ' --target-column-name '
+        if args.target_selection:
+            cmd += ' --target-selection ' + args.target_selection
+        if args.model_selection:
+            cmd += ' --model-selection ' + args.model_selection
+        if args.verbose: print(cmd)
         os.system(cmd)
-
+        rmsds_files.append(outfile)
+    # process the results
+    dfs = []
+    for f in rmsds_files:
+        df1 = pd.read_csv(f, index_col='fn')
+        dfs.append(df1)
+    df = pd.concat(dfs, axis=1)# join='inner')
+    df['mean'] = df.mean(numeric_only=True, axis=1)
+    df['min'] = df.min(numeric_only=True, axis=1)
+    df['max'] = df.max(numeric_only=True, axis=1)
+    df['sd'] = df.std(numeric_only=True, axis=1)
+    df.round(2).to_csv(args.output_csv)
+    print('-' * 80)
+    print(df.round(2))
+    print('-' * 80)
+    print('Save %s' % args.output_csv)
