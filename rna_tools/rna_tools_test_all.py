ec8e952a34c8a7acb5cedf36b6e3798e98198c59
Marcin Magnus <mag_dex@o2.pl>
2019-11-02 12:14:40 +0100

    Add quick test for installation: rna_tools_test_all.py

---
+++ rna_tools/rna_tools_test_all.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+from rna_tools.BlastPDB import BlastPDB
+from rna_tools.RfamSearch import RNASequence, RfamSearch
+from rna_tools.rna_tools_lib import RNAStructure
+import os
+from rna_tools.rna_tools_lib import edit_pdb, add_header, get_version, \
+                          collapsed_view, fetch, fetch_ba, replace_chain, RNAStructure, \
+                          select_pdb_fragment, sort_strings
+from rna_tools.tools.rna_x3dna.rna_x3dna import x3DNA
+
+def test_blastpdb():
+    p = BlastPDB('GGGUCAGGCCGGCGAAAGUCGCCACAGUUUGGGGAAAGCUGUGCAGCCUGUAACCCCCCCACGAAAGUGGG')
+    p.search()
+    assert p.result.startswith('<HTML') is True
+
+
+def test_rfamsearch():
+    seq = RNASequence("GGCGCGGCACCGUCCGCGGAACAAACGG")
+    rs = RfamSearch()
+    hit = rs.cmscan(seq)
+    print(hit)
+
+
+def test_rnastructre():
+    pass
+
+if __name__ == '__main__':
+    # get version
+    version = os.path.basename(os.path.dirname(os.path.abspath(__file__))), get_version(__file__)
+    version = version[1].strip()
+
+    print(version)
+
+    print('See full list of tools <https://github.com/mmagnus/rna-tools/blob/master/rna-tools-index.csv')
+    
+    print('OK')
+
+
+
+
+    
