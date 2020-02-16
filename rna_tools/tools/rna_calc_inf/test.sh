8f06daa6d8c63644faf7622094bbf2afd8e57365
Marcin Magnus <mag_dex@o2.pl>
2020-02-16 18:25:52 +0100

    rna_calc_inf.py: must use stacking otherwise there is an error in inf_all fix #102 + print results + sort results

--- rna_tools/tools/rna_calc_inf/test.sh
+++ rna_tools/tools/rna_calc_inf/test.sh
@@ -1,4 +1,9 @@
 #./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -o test_output/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb
 
 #((((.[[[[[[.))))........((((.....]]]]]]...(((((....)))))..))))'
-./rna_calc_inf.py -s pistol.ss -o test_output/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb #../rna_calc_rmsd/test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb
+./rna_calc_inf.py -s pistol.ss -o test_output/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb
+#../rna_calc_rmsd/test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb
+
+./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb --no-stacking -pr -o test_output/pistol_inf_nostacking.csv > test_output/pistol_inf_nostacking.log
+
+./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb -pr -o test_output/pistol_inf_stacking.csv > test_output/pistol_inf_stacking.log
