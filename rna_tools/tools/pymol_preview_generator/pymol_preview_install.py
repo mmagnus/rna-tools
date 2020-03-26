f8fc96cdf00c4c55400df4db4431df45f14c5df0
Marcin Magnus <mag_dex@o2.pl>
2020-03-26 16:43:56 +0100

    pymol_preview_generator: instalator add

--- rna_tools/tools/pymol_preview_generator/pymol_preview_install.py
+++ rna_tools/tools/pymol_preview_generator/pymol_preview_install.py
@@ -18,8 +18,5 @@ if __name__ == '__main__':
     parser = get_parser()
     args = parser.parse_args()
     p = rna_tools_lib.get_rna_tools_path() + '/tools/pymol_preview_generator/PyMOL Preview.workflow'
-    #cmd = "cp -rv '" + p + "' ~/Library/Services/"
-    cmd = 'curl https://raw.githubusercontent.com/mmagnus/rna-tools//master/rna_tools/tools/pymol_preview_generator/PyMOL%20Preview.workflow.zip -o ~/Library/Services/tmp.zip'
-    os.system(cmd)
-    cmd = "unzip ~/Library/Services/tmp.zip -d 'PyMOL Preview.workflow'"
+    cmd = "cp -rv '" + p + "' ~/Library/Services/"
     os.system(cmd)
