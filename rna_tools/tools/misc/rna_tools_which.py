e93afa63695b3d53303be415948682ec1c16e6bd
Marcin Magnus <mag_dex@o2.pl>
2020-04-07 13:07:14 +0200

    rna_tools_which.py: add --site, show the up folder for the pkg

--- rna_tools/tools/misc/rna_tools_which.py
+++ rna_tools/tools/misc/rna_tools_which.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-
 """
 import argparse
+import os
 
 def get_parser():
     parser = argparse.ArgumentParser(
         description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--site", action="store_true", help="be verbose")
     parser.add_argument("-v", "--verbose",
                         action="store_true", help="be verbose")
     return parser
@@ -18,4 +19,7 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     from rna_tools import rna_tools_lib
-    print(rna_tools_lib.get_rna_tools_path())
+    if args.site:
+        print(os.path.dirname(rna_tools_lib.get_rna_tools_path()))
+    else:
+        print(rna_tools_lib.get_rna_tools_path())
