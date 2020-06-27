70abee0cb3abe2782ecbcbe25c91a74d0a33fa8c
Marcin Magnus <mag_dex@o2.pl>
2020-06-27 17:42:17 +0200

    Fix problems with rna_calc_inf.py, re-write tests, add docs

--- rna_tools/tools/rna_calc_inf/test.sh
+++ rna_tools/tools/rna_calc_inf/test.sh
@@ -1,9 +1,7 @@
-#./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -o test_output/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb
-
+set -x
 #((((.[[[[[[.))))........((((.....]]]]]]...(((((....)))))..))))'
-./rna_calc_inf.py -s pistol.ss -o test_output/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb
-#../rna_calc_rmsd/test_data/pistol/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb
+rna_calc_inf.py -s test_data/pistol.fa -o test_data/pistol_inf.csv -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb
 
-./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb --no-stacking -pr -o test_output/pistol_inf_nostacking.csv > test_output/pistol_inf_nostacking.log
+rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb --no-stacking -pr -o test_data/pistol_inf_nostacking.csv > test_data/pistol_inf_nostacking.log
 
-./rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb -pr -o test_output/pistol_inf_stacking.csv > test_output/pistol_inf_stacking.log
+rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb -pr -o test_data/pistol_inf_nostacking.csv > test_data/pistol_inf_nostacking.log
