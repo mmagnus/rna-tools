4f39b6b29d1f75a47f44b191a8ae1e4870ff842f
Marcin Magnus <mag_dex@o2.pl>
2020-06-28 09:33:14 +0200

    rna_calc_inf.py: --renumber-residues

--- rna_tools/tools/rna_calc_inf/test.sh
+++ rna_tools/tools/rna_calc_inf/test.sh
@@ -5,3 +5,10 @@ rna_calc_inf.py -s test_data/pistol.fa -o test_data/pistol_inf.csv -f ../rna_cal
 rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb --no-stacking -pr -o test_data/pistol_inf_nostacking.csv > test_data/pistol_inf_nostacking.log
 
 rna_calc_inf.py -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb -pr -o test_data/pistol_inf_nostacking.csv > test_data/pistol_inf_nostacking.log
+
+rna_calc_inf.py --target-selection A:1-3+16-18 \
+                --model-selection A:1-3+16-18 \
+                -t ../rna_calc_rmsd/test_data/pistol/clusters/5k7c_clean_onechain_renumber_as_puzzle_srr.pdb \
+                -f ../rna_calc_rmsd/test_data/pistol/clusters/*.pdb \
+                -pr -o test_data/pistol_inf_nostacking.csv \
+                --renumber-residues
