399c7ae1cfc4ab1afc1838fbf7bca85e9047d105
Marcin Magnus <mag_dex@o2.pl>
2020-07-10 13:34:10 +0200

    digitifier.py: add initial version

--- rna_tools/tools/spotifier/test.sh
+++ rna_tools/tools/spotifier/test.sh
@@ -1,1 +1,2 @@
-python spotifier.py testdata/02/map.txt testdata/02/s02_30.jpg  --size 120
+# python spotifier.py testdata/02/map.txt testdata/02/s02_30.jpg  --size 120
+python digitifier.py 8 6 testdata/digitfier/1_all.png
