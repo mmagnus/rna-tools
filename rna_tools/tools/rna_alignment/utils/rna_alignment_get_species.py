a5584424ec3c38e7bdda3b76035fd53a2787ba81
Marcin Magnus <mag_dex@o2.pl>
2020-10-21 10:58:29 +0200

    rna_alignment_get_species.py: clean it, add one more test

--- rna_tools/tools/rna_alignment/utils/rna_alignment_get_species.py
+++ rna_tools/tools/rna_alignment/utils/rna_alignment_get_species.py
@@ -20,18 +20,7 @@ Examples 2::
     #=GF RT The spliceosomal snRNAs of Caenorhabditis elegans.
     #=GF TC 40.00
     #=GF RN [3]
-    #=GF RM 2339054
-    #=GF RL Nucleic Acids Res 1990;18:2633-2642.
-    #=GF AU Gardner PP; 0000-0002-7808-1213
-    #=GF CC methylation.
-    #=GF CB cmcalibrate --mpi CM
-    #=GF DR GO; 0046540; U4/U6 x U5 tri-snRNP complex;
-    #=GF ID U5
-    #=GF SS Published; PMID:2339054; Griffiths-Jones SR
-    #=GF RA Thomas J, Lea K, Zucker-Aprison E, Blumenthal T
-    #=GF SQ 180
-    #=GF SM cmsearch --cpu 4 --verbose --nohmmonly -E 1000 -Z 549862.597050 CM SEQDB
-    #=GF DE U5 spliceosomal RNA
+    (...)
     #=GF AC RF00020
     #=GF SE Zwieb C, The uRNA database, PMID:9016512; PMID:18390578
     #=GF GA 40.00
@@ -44,10 +33,6 @@ Examples 2::
     Oryctolagus-cuniculus-(rabbit)                     GAUC-GUAUAAAUCUUUCGCCUUUUACUAAAGA-UUUCCG----UGG-A--GA-G
     Cavia-porcellus-(domestic-guinea-pig)              GAUC-GUAUAAAUCUUUCGCCUUUUACUAAAGA-UUUCCG----UGG-A--GA-G
     Ochotona-princeps-(American-pika)                  GAUC-GUAUAAAUCUUUCGCCUUUUACUAAAGA-UUUCCG----UGG-A--GA-G
-
-.. note::
-
-  This code has way more code than the name of the script says. This is a customized script based on some script that did way more than getting species for a Stokholm file.
 
 """
 from __future__ import print_function
@@ -72,8 +57,6 @@ def get_parser():
     parser.add_argument('--evo-mapping')
     parser.add_argument('--evo-mapping-default', action="store_true")
     parser.add_argument('--one', action="store_true")
-    parser.add_argument('--u5', action="store_true")
-    parser.add_argument('--calc-energy', action="store_true")
     parser.add_argument('--osfn')
     parser.add_argument("alignment")
     return parser
@@ -147,22 +130,22 @@ def get_species(id, ocfn, verbose=False):
         df.to_csv(ocfn) # , index=False)
     return os, oc
 
-
-# some default simple mapping
-mapping =  [['Metazoa', 'Metazoa'],
-           ['Viridiplantae', 'Viridiplantae'],
-           ['Saccharomycotina', '(Fungi) Saccharomycotina'],
-           ['Euglenozoa', 'Euglenozoa'],
-           ['Alveolata', 'Alveolata'],
-           ['Amoebozoa', 'Amoebozoa'],
-           ['Cryptophyta', 'Cryptophyta'],
-           ['Parabasalia', 'Parabasalia'],
-           ['Rhizaria', 'Rhizaria'],
-           ['Stramenopiles', 'Stramenopiles'],
-           ['metagenomes', 'metagenomes'],
-           ]
-
+# main
 if __name__ == '__main__':
+    # some default simple mapping
+    mapping =  [['Metazoa', 'Metazoa'],
+               ['Viridiplantae', 'Viridiplantae'],
+               ['Saccharomycotina', '(Fungi) Saccharomycotina'],
+               ['Euglenozoa', 'Euglenozoa'],
+               ['Alveolata', 'Alveolata'],
+               ['Amoebozoa', 'Amoebozoa'],
+               ['Cryptophyta', 'Cryptophyta'],
+               ['Parabasalia', 'Parabasalia'],
+               ['Rhizaria', 'Rhizaria'],
+               ['Stramenopiles', 'Stramenopiles'],
+               ['metagenomes', 'metagenomes'],
+               ]
+
     parser = get_parser()
     args = parser.parse_args()
 
@@ -196,44 +179,7 @@ if __name__ == '__main__':
                 except:
                     print('Error in ', l)
                     sys.exit(1)
-                energy = ''
-                ss = ''
-                if False:
-                    ################################################################################
-                    a = ungap(seq[0:4])
-                    b = ungap(seq[-3:])
-                    ####################
-                    # to the new alignment send only a + b, overwrite the original sequence
-                    # print(seq)
-                    seq = a + '-' + b
-                    # print(seq)
 
-                if args.u5:
-                    a = ungap(seq[0:8])
-                    b = ungap(seq[-9:])
-                    seq = a + '-' + b
-
-                if args.calc_energy:
-                    # u5
-                    seql = RNASequence(seq.replace('-','').replace('N', 'u'))
-                    cst = "((((((((...........))))))))"
-                    #AAAUCUUUCGCCUUUUACUAAAGA-UUU
-                    #((((((((...........))))).)))
-                    energy, ss = seql.predict_ss(method="mcfold", constraints=cst, verbose=args.verbose)
-                    # u6
-                    #pass
-                    ## # u6atac
-                    ## a = 'g' + ungap(seq[0:5])
-                    ## b = ungap(seq[-4:]) + 'c'
-                    ## loop = "guaa"
-                    ## seql = RNASequence(a + loop + b)
-                    ## cst = '((((.((..))))))'
-                    ## if args.verbose:
-                    ##     print('seq %s' % seq)
-                    ##     print(a + loop + b)
-                    ##     print(cst)
-                    ## energy, ss = seql.predict_ss(method="mcfold", constraints=cst, verbose=args.verbose)
-                    ## if args.verbose: print(energy, ss)
                 ################################################################################
                 os, oc = get_species(id, args.osfn, args.verbose)
                 if not os:
@@ -263,30 +209,24 @@ if __name__ == '__main__':
                     #
                     #print((os.replace(' ', '-')[:name_width] + '[' + group.replace(' ','-') + ']' + semi_clean_id(id) + '|' + str(round(energy,2)) +
                     #       '|' + a + '-' + b + ss).ljust(name_width), seq.strip()) # clean_id(id)
-                    if energy:
-                        print((os.replace(' ', '-')[:name_width] + '[' + group.replace(' ','-') + ']' + semi_clean_id(id) + '|' + str(round(energy,2)) +
-                               '|' + ss).ljust(name_width), seq.strip()) # clean_id(id)
-                    else:
-                        print((group.replace(' ','-') + '|' + os.replace(' ', '-')[:name_width] + '|' + semi_clean_id(id) + ss).ljust(name_width), seq.strip())
+                    print((group.replace(' ','-') + '|' + os.replace(' ', '-')[:name_width] + '|' + semi_clean_id(id) + ss).ljust(name_width), seq.strip())
 
                 else:
                     # this is no group
                     # ech, this is super messy, sorry!
                     group = ''
-                    print((os.strip().replace(' ', '-')[:name_width] + str(energy)).ljust(name_width), seq.strip())
+                    print((os.strip().replace(' ', '-')[:name_width] + str('')).ljust(name_width), seq.strip())
 
                 if args.one:
                     break
-                #else:
-                #    print(os.replace(' ', '-') + str(energy)[:name_width].ljust(name_width), seq.strip())
 
             elif '#=GC SS_cons' in l:
                 ss = l.replace('#=GC SS_cons', '')
                 print('#=GC SS_cons'.ljust(name_width), ss.strip())
-
             # OK, i'm not sure, if this should be RF_cons or RF
             elif '#=GC RF_cons' in l:
                 ss = l.replace('#=GC SS_cons', '')
+                ss = l.replace('#=GC RF_cons', '')
                 print('#=GC RF_cons'.ljust(name_width), ss.strip())
             elif '#=GC RF' in l:
                 ss = l.replace('#=GC RF', '')
--- rna_tools/tools/rna_alignment/utils/test.sh
+++ rna_tools/tools/rna_alignment/utils/test.sh
@@ -3,4 +3,6 @@ set -v
 ./mfa-get-seq-for-species.py test_data/RF00026_demo.fa Mustela test_data/mustela.fa
 ./mfa-get-seq-for-species.py --format test_data/RF00026_demo.fa Mustela test_data/mustela_format.fa
 ./rna_align_strip_stk.py test_data/mutant_list.stk > test_data/mutant_list.txt
+
 ./rna_alignment_get_species.py test_data/u5_rfam_u5only.stk
+./rna_alignment_get_species.py test_data/u5_rfam_u5only.stk --one
