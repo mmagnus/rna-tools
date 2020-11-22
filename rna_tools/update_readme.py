943105da8b3ac795f24658da3f4cb38962494878
Marcin Magnus <mag_dex@o2.pl>
2020-11-22 18:19:28 +0100

    Fix update_readme.py

--- rna_tools/update_readme.py
+++ rna_tools/update_readme.py
@@ -4,24 +4,24 @@
 import subprocess
 
 if __name__ == '__main__':
-    start_tag = "usage: rna_pdb_toolsx.py [-h] [--version] [-r] [--renum-atoms]"
+    start_tag = "usage: rna_pdb_toolsx.py"
     end_tag = "Tricks:"
 
     #
     doc = subprocess.Popen('python rna_pdb_toolsx.py -h', shell=True, stdout=subprocess.PIPE).stdout.read().decode()
-    fn = ''
+    txt = ''
     f = open('../README.md', 'r')
     copy_line_flag = True
     for l in f:
-        if copy_line_flag:
-            fn += l
-        if l.startswith(start_tag):
-            fn += doc + '\n'
-            copy_line_flag = False
         if l.strip().startswith(end_tag):
             copy_line_flag = True
+        if l.startswith(start_tag):
+            txt += doc + '```\n\n'
+            copy_line_flag = False
+        if copy_line_flag:
+            txt += l
 
     f = open('../README.md', 'w')
-    f.write(fn)
-    print(fn)
+    f.write(txt)
+    print(txt)
     f.close()
