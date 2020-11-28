2b30a644776444e4b7a4639db41c47204212c694
Marcin Magnus <mag_dex@o2.pl>
2020-11-28 19:20:50 +0100

    mq: RNA3DCNN.py returns also local scores

--- rna_tools/tools/mq/RNA3DCNN/RNA3DCNN.py
+++ rna_tools/tools/mq/RNA3DCNN/RNA3DCNN.py
@@ -1,8 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This module contains functions for computing RASP potential
+"""This module contains functions for computing RNA3DCNN potential
+
+Output::
+
+    Trainable params: 4,282,801
+    Non-trainable params: 0
+    _________________________________________________________________
+    Scores for each nucleotide in /var/folders/yc/ssr9692s5fzf7k165grnhpk80000gp/T/tmptg6jy2ud/query.pdb:
+    [[ 0.02462262]
+     [ 0.03271335]
+     [ 0.06199259]
+     [ 0.02006263]
+     [ 0.05937254]
+     [ 0.12025979]
+     [ 0.20201728]
+     [ 0.24463326]
+     [ 0.43518737]
+     [ 0.7260638 ]
+     [ 0.6140108 ]
+     [ 0.6588027 ]
+     [ 0.7668936 ]
+     [ 0.4776191 ]
+     [ 0.39859247]
+     [ 0.572009  ]
+     [ 0.64892375]
+     [ 0.11587611]
+     [ 0.0560993 ]
+     [ 0.05285829]
+     [ 0.0167731 ]
+     [ 0.01759553]
+     [ 0.02143204]
+     [-0.01818037]]
+    Total score for /var/folders/yc/ssr9692s5fzf7k165grnhpk80000gp/T/tmptg6jy2ud/query.pdb is  6.3262305
+
 """
 import os
+import re
 from shutil import copyfile
 from rna_tools.tools.mq.lib.wrappers.SubprocessUtils import run_command
 from rna_tools.tools.pdb_formatix.PDBFile import PDBFile#resname_check_and_3to1, set_residues_bfactor
@@ -16,8 +50,8 @@ class RNA3DCNN(ProgramWrapper):
     """
     max_seq_len = 100000  # I don't know about any restriction
 
-    def __init__(self, sequence='', seq_name='', job_id=None):
-        super(RNA3DCNN, self).__init__(sequence, seq_name, job_id=job_id)
+    def __init__(self):
+        super(RNA3DCNN, self).__init__()
 
     def run(self, path_to_pdb, verbose=False):
         copyfile(path_to_pdb, self.sandbox_dir + os.sep + 'query.pdb')
@@ -34,7 +68,7 @@ class RNA3DCNN(ProgramWrapper):
         cmd = PYTHON3_PATH + ' ' + RNA3DCNN_PATH + '/Main.py ' + \
         ' -pn ' + self.sandbox_dir + '/query.pdb ' + \
         ' -model ' + RNA3DCNN_PATH + '/RNA3DCNN_MD.hdf5 ' + \
-        ' -local 0 2>>  '  + self.sandbox_dir + '/log.txt >>' + self.sandbox_dir + '/log.txt'
+        ' -local 1 2>>  '  + self.sandbox_dir + '/log.txt >>' + self.sandbox_dir + '/log.txt'
         if verbose:
             print(cmd)
         os.system(cmd)
@@ -47,8 +81,14 @@ class RNA3DCNN(ProgramWrapper):
         Total score for /var/folders/yc/ssr9692s5fzf7k165grnhpk80000gp/T/tmpO_jRVR/query.pdb is  6.3262305
         """
         self.log('Run finished')
-        score = open(self.sandbox_dir + '/log.txt').read().split()[-1]
+        output = open(self.sandbox_dir + '/log.txt').read()
+        score = output.split()[-1]
+        lscore = re.search('\[\[.*\]\]', output, re.M | re.DOTALL)
+        lscore = lscore.group().replace('[','').replace(']\n', ',').replace(']]','')
+        # 0.02462262,  0.03271335,  0.06199259,  0.02006263,  0.05937254,  0.12025979, 
+        lscore = [float(x.strip()) for x in lscore.split(',')] # [0.02462262, 0.03271335, ...
         if verbose:
+            print(lscore)
             print(score)
         os.chdir(old_pwd)
         try:
@@ -60,9 +100,9 @@ class RNA3DCNN(ProgramWrapper):
             return error
 
 def main():
-    wrapper = RNA3DCNN('', '')
+    wrapper = RNA3DCNN()
     try:
-        result = wrapper.run('test' + os.sep + '1a9n.pdb')
+        result = wrapper.run('../test' + os.sep + '1a9n.pdb', verbose=True)
         if result:
             print(result)
     except Exception as e:
