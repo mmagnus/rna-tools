06362909cd736b51038e7fcde8046e8514a87ca0
Marcin Magnus <mag_dex@o2.pl>
2021-03-04 14:19:36 +0100

    spotifier_prep.py: if 0 in mapping, insert gray square

--- rna_tools/tools/spotifier/spotifier2.py
+++ rna_tools/tools/spotifier/spotifier2.py
@@ -210,8 +210,15 @@ if __name__ == '__main__':
             row_fig_y = 0
             for s in row:  # spot in row
                 # for center something like this https://stackoverflow.com/questions/1970807/center-middle-align-text-with-pil
-                fig.paste(spots[s - 1], (x, y)) # s - 1 # index from 0
-                row_fig.paste(spots[s - 1], (x, row_fig_y))
+                if s == 0: # put a gray box here
+                    img = Image.new('RGB', (100, 100))
+                    draw = ImageDraw.Draw(img)
+                    draw.rectangle((0,0,100,100), fill ="#808080") # , outline ="red")
+                    fig.paste(img, (x, y)) # s - 1 # index from 0
+                    row_fig.paste(img, (x, row_fig_y))
+                else:
+                    fig.paste(spots[s - 1], (x, y)) # s - 1 # index from 0
+                    row_fig.paste(spots[s - 1], (x, row_fig_y))
                 spots_text += ' ' + str(s)
                 x += 100
             # run it for the whole row
