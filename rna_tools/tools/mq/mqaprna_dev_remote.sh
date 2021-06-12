93b0bbd413004ffceedc880c8ad528492c64eb1d
Marcin Magnus <mag_dex@o2.pl>
2021-06-12 11:53:05 +0200

    Dirrty script to run mq on my VM machine: mqaprna_dev_remote.sh

---
+++ rna_tools/tools/mq/mqaprna_dev_remote.sh
@@ -0,0 +1,5 @@
+folder=job`openssl rand -hex 12`
+ssh mq mkdir $folder
+rsync --delete --include '*.pdb' --exclude="*" -rv $1/* mq:$folder
+ssh mq "cd $folder && /home/mqapRNA/mqaprna_env/mqapRNA/main/mqaprna * -o mq.csv"
+rsync -r  --exclude='*.pdb'  --exclude='.csvsort*'  mq:$folder/ $1
