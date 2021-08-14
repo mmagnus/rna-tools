601eba3dfa1fdda7451a474235834e4a0dc597d8
mmagnus <magnus@genesilico.pl>
2021-08-14 12:05:04 +0200

    mq/rasp: refactor test

--- rna_tools/tools/mq/RASP/test.py
+++ rna_tools/tools/mq/RASP/test.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 import os
 from rna_tools.tools.mq.RASP.RASP import RASP
-from rna_tools.rna_tools_config import WRAPPERS_PATH, TEST_DATA
-
 
 def main():
-    wrapper = RASP('', '')
+    wrapper = RASP()
     try:
-        result = wrapper.run(TEST_DATA + os.sep + '4ts2.pdb')
+        result = wrapper.run('../../../input/4ts2.pdb')
         if result:
             print(result)
     except Exception as e:
