b17b202fb88e59f26a42b0b34bf36d8fb4c0236b
Marcin Magnus <mag_dex@o2.pl>
2021-10-30 09:29:41 +0200

    mq: FARNA re-born?

---
+++ rna_tools/tools/mq/FARNA/rna_mq_farna.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+"""
+from __future__ import print_function
+import argparse
+from rna_tools.tools.mq.FARNA.FARNA import FARNA
+import os
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    parser.add_argument("-r", "--hires",
+                        action="store_true", help="")
+    parser.add_argument("file", help="", default="", nargs='+')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if list != type(args.file):
+        args.file = [args.file]
+
+    if args.hires:
+        print('id,fn,' + ','.join(['farna_atom_pair_constraint_hires', 'farna_score_hires', 'farna_hbond_sr_bb_sc', 'farna_linear_chainbreak_hires', 'farna_fa_intra_rep', 'farna_rna_sugar_close', 'farna_hbond_sc', 'farna_lk_nonpolar', 'farna_fa_atr', 'farna_fa_elec_rna_phos_phos', 'farna_ch_bond', 'farna_fa_rep', 'farna_hbond_lr_bb_sc', 'farna_geom_sol', 'farna_rna_torsion']))
+    else:
+        print('id,fn,' + ','.join(['farna_rna_base_axis', 'farna_rna_backbone_backbone', 'farna_rna_base_stack_axis', 'farna_rna_base_stagger', 'farna_rna_base_stack', 'farna_rna_base_pair', 'farna_rna_repulsive', 'farna_rna_vdw', 'farna_rna_base_backbone', 'farna_score_lowres', 'farna_rna_data_backbone', 'farna_linear_chainbreak', 'farna_rna_rg', 'farna_atom_pair_constraint']))
+    for i, f in enumerate(args.file):
+        # mini false
+        # print(f)
+        farna = FARNA()
+        result = farna.run(f, args.hires, args.verbose)  # False or True for min
+        print(','.join([str(i + 1), os.path.basename(f)]) + ',' + ','.join(result))
