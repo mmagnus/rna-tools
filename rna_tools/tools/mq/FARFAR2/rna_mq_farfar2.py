ebc564e9e0e84d22e77ee9d24cc7d79d7d291574
magnus <magnus@m1.local>
2021-11-30 14:07:08 +0100

    mq: ff2 cmd tool no need for pandas

--- rna_tools/tools/mq/FARFAR2/rna_mq_farfar2.py
+++ rna_tools/tools/mq/FARFAR2/rna_mq_farfar2.py
@@ -7,7 +7,6 @@ from __future__ import print_function
 import argparse
 from rna_tools.tools.mq.FARFAR2.FARFAR2 import FARFAR2
 import os
-import pandas as pd
 
 def get_parser():
     parser = argparse.ArgumentParser(
