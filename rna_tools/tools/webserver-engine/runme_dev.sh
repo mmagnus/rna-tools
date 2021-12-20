f250d5c6c33e7c2d4f4c92e2d2ddbdc5d02fcf74
Marcin Magnus <mag_dex@o2.pl>
2021-12-21 00:13:23 +0100

    web: runme_dev.sh kill at 8080

--- rna_tools/tools/webserver-engine/runme_dev.sh
+++ rna_tools/tools/webserver-engine/runme_dev.sh
@@ -4,5 +4,5 @@
 #ABSPATH=$(cd "$(dirname "$0")"; pwd)
 #. $ABSPATH/../bin/activate
 source rna_tools_env/bin/activate
-#kill -9 `lsof -t -i:80`
+kill -9 `lsof -t -i:8080`
 python manage.py runserver --settings web.settings  0:8080
