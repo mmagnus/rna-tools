8b2fec762bb680db9a46191c36b7041c2b169d5c
magnus <magnus@m1.local>
2021-12-27 22:39:08 +0100

    web: view file on the same page

--- rna_tools/tools/webserver-engine/app/views.py
+++ rna_tools/tools/webserver-engine/app/views.py
@@ -417,7 +417,9 @@ def ajax_job_status(request, job_id, tool=''):
            for f in files:
                bf = os.path.basename(f)
                # The raw output files for each step of the pipeline can be found <a href="{{ {{ j.job_id }}.zip">here</a>
-               bfl = '<a target="_blank" href="/media/jobs/' + job_id + '/' + bf +'">' + bf + '</a>'
+               # target="_blank" # for PDB files it open an empty page
+               # this is not needed
+               bfl = '<a href="/media/jobs/' + job_id + '/' + bf +'">' + bf + '</a>'
                log += '<a href="#" onclick="del(\'' + job_id + '/' + bf + '\');">x</a> ' +  bfl + '</br>'
         # log += "</br>== FILES END ==</br>"
 
