6650b813abc065aa5995dd2485c75370a6f5ab24
mmagnus <mag_dex@o2.pl>
2022-01-31 18:24:39 +0100

    trx: clean up

--- rna_tools/tools/triplexibility/triples1-1/run.sh
+++
@@ -1,6 +0,0 @@
-set -x
-#  --debug 
-#../../rna_calc_rmsd_biopython.py -t spl-1-1.pdb *.pdb --save --way backbone+sugar  --triple-mode --sort --result rmsd.csv # --ignore-files CGA
-# --save
-../../rna_calc_rmsd_biopython.py -t spl-1-1.pdb triples-all-v2-rpr/*.pdb  --way backbone+sugar --triple-mode --sort --result rmsd.csv
-# --ignore-files CGA
