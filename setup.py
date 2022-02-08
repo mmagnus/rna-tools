5c5106ddeda6ac10fa3994c508f5f481bb235439
mmagnus <mag_dex@o2.pl>
2022-02-08 13:06:04 +0100

    trx_mutgen.py: add

--- setup.py
+++ setup.py
@@ -134,6 +134,7 @@ setup(
              
              'rna_tools/tools/triplexibility/triplexibility.py',
              'rna_tools/tools/triplexibility/triplexibility2.py',
+             'rna_tools/tools/triplexibility/trx_mutgen.py',
              
              'rna_tools/tools/misc/rna_merge_two_dfs.py',
              'rna_tools/tools/misc/rna_merge_dfs.py',
