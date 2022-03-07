b5ce5a65bdd5cd02ae86107630898a86457fbfe8
mmagnus <mag_dex@o2.pl>
2022-03-07 11:55:43 +0100

    up triplexibility/db/gendb.py

--- rna_tools/tools/triplexibility/db/gendb.py
+++ rna_tools/tools/triplexibility/db/gendb.py
@@ -6,6 +6,6 @@ for s in ['t1-3_cWW_tHW_UAA_exemplar_rpr', 't2-3-UAU', 't2-4-ACA', 't3-3_AUA']:
                   'Triple_*ex*_rpr.pdb',
                   'Triple_cWW_*ex*_rpr.pdb',
                   ]: #'Triple_*_rpr.pdb']:
-            cmd = 'triplexibility.py triples-all-v2-rpr/%s -t %s.pdb --way %s --save --result %s_%s_%s_rmsd.csv --sort --triple-mode' % (f,s,mode,s,f,mode)
+            cmd = 'triplexibility.py triples-all-v2-rpr/%s -t %s.pdb --way %s --save --result %s_%s_%s_rmsd.csv --sort --folder-prefix %s_%s --triple-mode' % (f,s,mode,s, f.replace('*','X'),mode, f.replace('*','X'),mode)
             print(cmd)
             os.system(cmd)
