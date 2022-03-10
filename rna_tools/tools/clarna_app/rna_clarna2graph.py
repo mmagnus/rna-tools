9dcc9929db821a610be76d49866ad42520c092b1
mmagnus <mag_dex@o2.pl>
2022-03-10 14:36:01 +0100

    rna_clarna2graph.py: add a new tool

---
+++ rna_tools/tools/clarna_app/rna_clarna2graph.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+
+Example::
+
+    $ rna_clarna2graph.py cl.txt
+      source target    n
+    0     21     61  408
+    1     80     85   24
+    2     61     80    9
+    > out: 'cl_short.csv'
+
+and with ```--full```::
+
+       source target     type    n
+    0      21     61   WW_cis  405
+    1      80     85   WW_cis   13
+    2      80     85  HW_tran    6
+    3      80     85  WH_tran    5
+    4      61     80  WS_tran    4
+    5      61     80   HS_cis    3
+    6      21     61   WH_cis    1
+    7      61     80   WS_cis    1
+    8      21     61   SW_cis    1
+    9      61     80   HW_cis    1
+    10     21     61  SH_tran    1
+    > out: 'cl_full.csv'
+
+
+"""
+from __future__ import print_function
+import argparse
+from icecream import ic
+import sys
+ic.configureOutput(outputFunction=lambda *a: print(*a, file=sys.stderr))
+ic.configureOutput(prefix='> ')
+
+
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    parser.add_argument('-f', "--full",  action="store_true", help="split interactions into types")
+    parser.add_argument("-v", "--verbose",
+                        action="store_true", help="be verbose")
+    parser.add_argument("file", help="", default="") # nargs='+')
+    return parser
+
+
+if __name__ == '__main__':
+    parser = get_parser()
+    args = parser.parse_args()
+    full = args.full
+    v = args.verbose
+    
+    all = {}
+    for line in open(args.file):
+        if line.strip():
+            if v: ic(l.strip())
+            l = line.split()
+            if len(l) == 9:
+                # ['B', '21', 'A', '61', 'bp', 'U', 'A', 'WW_cis', '0.8365']
+                if full: # ('21-61-WW_cis', 405)
+                    k = l[1] + '-' + l[3] + '-' + l[7]
+                else:
+                    k = l[1] + '-' + l[3] # only nodes ('21-61', 408)
+                if k in all:  # count endge
+                    all[k] += 1
+                else:
+                    all[k] = 1
+
+    # sort it
+    import operator
+    alls = sorted(all.items(), key=operator.itemgetter(1), reverse=True)
+    nlst = []
+    for i in alls:
+        if v: ic(i)
+        ne = i[0].split('-') + [i[1]]  # [['21', '61', 408], ['80', '85', 24], ['61', '80', 9]]
+        nlst.append(ne)
+    if v: ic(nlst)
+
+    import pandas as pd
+    if full:
+        columns=['source', 'target', 'type', 'n']
+    else:
+        columns=['source', 'target', 'n']
+    df = pd.DataFrame(nlst, columns = columns)
+    print(df)
+
+    if full:
+        out = args.file.replace('.txt', '_full.csv')
+    else:
+        out = args.file.replace('.txt', '_short.csv')
+
+    ic(out)
+    df.to_csv(out)
--- rna_tools/tools/clarna_app/test.sh
+++ rna_tools/tools/clarna_app/test.sh
@@ -1,1 +1,3 @@
 ./clarna_app.py ../../input/pistol_thrs0.50A_clust99-000001_AA.pdb
+python rna_clarna2graph.py data/cl.txt
+python rna_clarna2graph.py --full data/cl.txt
