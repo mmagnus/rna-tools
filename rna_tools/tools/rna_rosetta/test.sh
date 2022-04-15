cce04ee4723d6989d9a6bbd90b62ff9b2145a0ab
mmagnus <mag_dex@o2.pl>
2022-04-15 22:03:00 +0200

    rna_rosetta_silent_split.py: add

--- rna_tools/tools/rna_rosetta/test.sh
+++ rna_tools/tools/rna_rosetta/test.sh
@@ -1,2 +1,3 @@
-./rna_rosetta_cluster.py test_data/selected.out 10
-./rna_rosetta_run.py -r -g -n 10 -c 1 --sandbox test_data test_data/tetraloop.fa
+#./rna_rosetta_cluster.py test_data/selected.out 10
+#./rna_rosetta_run.py -r -g -n 10 -c 1 --sandbox test_data test_data/tetraloop.fa
+python rna_rosetta_silent_split.py -o test_data test_data/selected.out
