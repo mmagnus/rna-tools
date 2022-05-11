b978f6fb3d16f93eb459008bc37bf149bea58084
mmagnus <mag_dex@o2.pl>
2022-05-11 19:26:34 +0000

    shell: up

--- rna_tools/shell_rc.sh
+++ rna_tools/shell_rc.sh
@@ -2,9 +2,19 @@
 # e.g., source ~/work/src/rna-tools/rna_tools/shell_rc.sh
 
 rna_rosetta_progress(){
-    parallel 'echo {} && cd {} && rna_rosetta_n.py default.out' ::: `ls -d */`
+    parallel 'echo {} && cd {} && rna_rosetta_n.py *.out' ::: `ls -d */`
 }
 
 rna_rosetta_run_for_folders(){
  parallel 'cd {} && ./*.sh ' ::: *
 }
+
+#
+#  shell-aliases
+#  install in .bashrc/.zshrc
+#
+#  source "/home/magnus/src/rna-pdb-tools/rna_pdb_tools/shell-aliases.sh"
+#
+
+alias rna_rosetta_n_loop="find . -maxdepth 2 -iname '*out' -type f -not -name 'helix*' -exec rna_rosetta_n.py --verbose {} \; | tee looplog.txt && echo 'sorted' && cat looplog.txt | sort"
+alias duu='du -s -B G * 2> /dev/null | sort -nr | tee duu' #du -h -d 1'
