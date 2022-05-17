d72d6f81729bfa6e07b2b4b1fe9c9ab46f8358ec
mmagnus <mag_dex@o2.pl>
2022-05-17 14:31:18 +0200

    shell: up

--- rna_tools/shell_rc.sh
+++ rna_tools/shell_rc.sh
@@ -2,9 +2,17 @@
 # e.g., source ~/work/src/rna-tools/rna_tools/shell_rc.sh
 
 rna_rosetta_progress(){
-    parallel 'echo {} && cd {} && rna_rosetta_n.py default.out' ::: `ls -d */`
+    parallel 'echo {} && cd {} && rna_rosetta_n.py *.out' ::: `ls -d */`
 }
 
 rna_rosetta_run_for_folders(){
- parallel 'cd {} && ./*.sh ' ::: *
+    parallel 'cd {} && ./*.sh ' ::: *
 }
+
+
+trx(){
+    rna_rosetta_extract_lowscore_decoys.py 10 *out
+    parallel rna_mq_farfar2.py -qr {} ::: *out*.pdb | tee ff2.csv
+}
+
+alias ta="tmux attach"
