bb6841607648b32d268433635c1df72e5ac8f90d
mmagnus <mag_dex@o2.pl>
2022-05-19 08:56:20 +0000

    web: fix a problem when 'contact' was in a file name ;-)

--- rna_tools/tools/webserver-engine/web/urls.py
+++ rna_tools/tools/webserver-engine/web/urls.py
@@ -8,35 +8,37 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'doc', 'app.views.help'),        
-    url(r'contact', 'app.views.contact'),
-    url(r'about', 'app.views.about'),
     url(r'.well-known/pki-validation/(?P<fn>.*)$', 'app.views.ssl'),
     url(r'2coejc8r22i2/(?P<fn>.*)$', 'app.views.notes'),
     url(r'4j6scj6p82zw400/(?P<fn>.*)$', 'app.views.qr'),
-    url(r'images/(?P<fn>.*)$', 'app.views.image'),
+    url(r'^images/(?P<fn>.*)$', 'app.views.image'),
 
-    url(r'fetch/(?P<job_id>.*)$','app.views.fetch', name='fetch'),
-    url(r'del/(?P<job_id_fn>.*)$','app.views.ajax_rm_file', name='ajax_rm_file'),
-    url(r'jobstatus/(?P<job_id>.*)/(?P<tool>.*)$', 'app.views.ajax_job_status', name='ajax_job_status'),
-    url(r'jobstatus/(?P<job_id>.*)$','app.views.ajax_job_status', name='ajax_job_status'),
+    url(r'^fetch/(?P<job_id>.*)$','app.views.fetch', name='fetch'),
+    url(r'^del/(?P<job_id_fn>.*)$','app.views.ajax_rm_file', name='ajax_rm_file'),
+    url(r'^jobstatus/(?P<job_id>.*)/(?P<tool>.*)$', 'app.views.ajax_job_status', name='ajax_job_status'),
+    url(r'^jobstatus/(?P<job_id>.*)$','app.views.ajax_job_status', name='ajax_job_status'),
 
-    url(r'download_project_dir/(?P<job_id>.*)$','app.views.download_project_dir', name='download_project_dir'),
-    url(r'stop/(?P<job_id>.*)$','app.views.stop'),
+    url(r'^download_project_dir/(?P<job_id>.*)$','app.views.download_project_dir', name='download_project_dir'),
+    url(r'^stop/(?P<job_id>.*)$','app.views.stop'),
 
-    url(r'download_project_dir/(?P<job_id>.*)$', 'app.views.download_project_dir', name='download_project_dir'),
-    url(r'stop/(?P<job_id>.*)$', 'app.views.stop'),
+    url(r'^download_project_dir/(?P<job_id>.*)$', 'app.views.download_project_dir', name='download_project_dir'),
+    url(r'^stop/(?P<job_id>.*)$', 'app.views.stop'),
 
     url(r'^tools/(?P<tool>.*)/(?P<job_id>.*)$','app.views.tool'),
     url(r'^demo/(?P<tool>.*)/(?P<job_id>.*)$','app.views.demo'),
     url(r'^tools','app.views.tools'),
                        
+    url(r'^doc', 'app.views.help'),        
+    url(r'^contact', 'app.views.contact'),
+    url(r'^about', 'app.views.about'),
+
     url(r'%s/*$' %  settings.server_name_py, 'app.views.home', name='home'),
     url(r'%s' %  settings.server_name_py, 'app.views.home', name='home'),
     url(r'^$', 'app.views.home', name='home'),
     url(r'admin/', include(admin.site.urls)),
     url('upload/(?P<job_id>.*)$', 'app.views.file_upload'),
-    url('run/(?P<tool>.*)/(?P<job_id>.*)$', 'app.views.run'),
+    url('^run/(?P<tool>.*)/(?P<job_id>.*)$', 'app.views.run'),
+
     # url(r'^web/', include('web.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
