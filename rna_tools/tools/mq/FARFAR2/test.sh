2a71e720c64fd1809a767c4feec9b2ec63b7ddda
mmagnus <mag_dex@o2.pl>
2022-08-01 11:59:30 +0200

    mq: ff2 clean up/refactor

--- rna_tools/tools/mq/FARFAR2/test.sh
+++ rna_tools/tools/mq/FARFAR2/test.sh
@@ -1,4 +1,6 @@
-rm test/*log
-#./rna_mq_farfar2.py test/3e5f_output4_01-000001_AA+ResnShift.pdb | tee test/farna.csv # && open test/farna.csv
-./rna_mq_farfar2.py -r test/3e5f_output4_01-000001_AA+ResnShift.pdb | tee test/farna_hires.csv # && open test/farna_hires.csv
-#../rna_mq_collect.py -f test/3e*.pdb -t FARFAR2_hires -o test/rna_mq_collect.csv
+rm test_output/*
+./rna_mq_farfar2.py ../test/1y26X_output4_01-000001_AA.pdb | tee test_output/ff2.csv # && open test/farna.csv
+./rna_mq_farfar2.py -r ../test/1y26X_output4_01-000001_AA.pdb | tee test_output/ff2_hires.csv # && open test/farna_hires.csv
+
+../rna_mq_collect.py -f ../test/1y26X_output4_01-000001_AA.pdb -t FARFAR2_hires -o test_output/ff2_hires_rna_mq_collect.csv
+../rna_mq_collect.py -f ../test/1y26X_output4_01-000001_AA.pdb -t FARFAR2 -o test_output/ff2_rna_mq_collect.csv
