e1a2065747009f979ee03c529b38a8ecfe286a6e
mmagnus <mag_dex@o2.pl>
2022-08-11 15:20:39 +0200

    add misc/rna_tools_demo.py

---
+++ rna_tools/tools/misc/rna_tools_demo.py
@@ -0,0 +1,13 @@
+from zipfile import ZipFile
+try:
+    import wget
+except:
+    print('wget missing, please "pip install wget"')
+    sys.exit(1)
+             
+url = "http://rna-tools.online/static/app/demo/rp21.zip"
+f = wget.download(url)
+fn = 'rp21.zip'
+with ZipFile(fn, 'r') as zip:
+    zip.printdir()
+    zip.extractall()
