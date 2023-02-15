9a72704594a5b9235247435e12f6879d1b6cd54f
mmagnus <mag_dex@o2.pl>
2023-02-15 18:44:57 -0500

    add stats.py

---
+++ rna_tools/stats.py
@@ -0,0 +1,13 @@
+G_ATOMS = "P OP1 OP2 O5' C5' C4' O4' C3' O3' C2' O2' C1' N9 C8 N7 C5 C6 O6 N1 C2 N2 N3 C4".split()
+A_ATOMS = "P OP1 OP2 O5' C5' C4' O4' C3' O3' C2' O2' C1' N9 C8 N7 C5 C6 N6 N1 C2 N3 C4".split()
+U_ATOMS = "P OP1 OP2 O5' C5' C4' O4' C3' O3' C2' O2' C1' N1 C2 O2 N3 C4 O4 C5 C6".split()
+C_ATOMS = "P OP1 OP2 O5' C5' C4' O4' C3' O3' C2' O2' C1' N1 C2 O2 N3 C4 N4 C5 C6".split()
+
+d = {'P' : 0,
+     'O' : 0,
+     'C' : 0,
+     'N' : 0}
+for i in G_ATOMS + A_ATOMS + U_ATOMS + C_ATOMS:
+    d[i[0]] += 1
+print(d)
+    
--- rna_tools/test.sh
+++ rna_tools/test.sh
@@ -1,4 +1,5 @@
 #pytest test_all.py --doctest-modules  --cov
 #pytest BlastPDB.py test.py RfamSearch.py SecondaryStructure.py Seq.py --doctest-modules
 #python BlastPDB.py
-python rna_tools_lib.py
+#python rna_tools_lib.py
+python stats.py
