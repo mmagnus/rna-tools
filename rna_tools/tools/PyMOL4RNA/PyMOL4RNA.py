a9ac7916fca57a07d7ce75b5e0e5fc9681740d2c
mmagnus <mag_dex@o2.pl>
2023-04-21 11:01:09 -0400

    PyMOL4RNA.py: PyMOL4RNA.py: draw axes - correct one

--- rna_tools/tools/PyMOL4RNA/PyMOL4RNA.py
+++ rna_tools/tools/PyMOL4RNA/PyMOL4RNA.py
@@ -1164,6 +1164,28 @@ def se(): # save
 cmd.extend('se', se)
 print('se - quick save enabled to tmp.pdb')
       
+
+from pymol.cgo import *
+
+def axes():
+    """Draw XYZ
+    https://pymolwiki.org/index.php/Axes
+    """
+    print('Draw axis red, green blue for X, Y and Z')
+    w = 0.06 # cylinder width 
+    l = 0.75 # cylinder length
+    h = 0.25 # cone hight
+    d = w * 1.618 # cone base diameter
+
+    obj = [CYLINDER, 0.0, 0.0, 0.0,   l, 0.0, 0.0, w, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
+           CYLINDER, 0.0, 0.0, 0.0, 0.0,   l, 0.0, w, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0,
+           CYLINDER, 0.0, 0.0, 0.0, 0.0, 0.0,   l, w, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0,
+           CONE,   l, 0.0, 0.0, h+l, 0.0, 0.0, d, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 
+           CONE, 0.0,   l, 0.0, 0.0, h+l, 0.0, d, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 
+           CONE, 0.0, 0.0,   l, 0.0, 0.0, h+l, d, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0]
+
+    cmd.load_cgo(obj, 'axes')
+cmd.extend('axes', axes)
 def ha():
     """
         cmd.do('h_add')
