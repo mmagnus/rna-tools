41c0d7724d9c98cad9e40371610d86c6baf64fa9
mmagnus <mag_dex@o2.pl>
2025-11-03 12:19:22 -0500

    rna_calc_evo_rmsd.py - several updates
    - the mapping file flag is optional filenames in pdb files the same as in alignments
    - target filename’s basename used when --target_name isn’t given
    - improve docs

--- rna_tools/tools/rna_calc_evo_rmsd/test.sh
+++ rna_tools/tools/rna_calc_evo_rmsd/test.sh
@@ -1,1 +1,2 @@
-./rna_calc_evo_rmsd.py -a test_data/aln.sto -t test_data/4qk8_cl.pdb test_data/4qlm_cl.pdb -n 4qk8_cl -m test_data/mapping.txt
+#./rna_calc_evo_rmsd.py -a test_data/aln.sto -t test_data/4qk8_cl.pdb test_data/4qlm_cl.pdb -n 4qk8_cl -m test_data/mapping.txt
+./rna_calc_evo_rmsd.py -a test_data/aln.sto -t test_data/4qk8_cl.pdb test_data/4qlm_cl.pdb
