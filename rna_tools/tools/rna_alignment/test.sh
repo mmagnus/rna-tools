88956990dc80a09685fd6b571d0b653a16451c29
mmagnus <mag_dex@o2.pl>
2026-02-04 22:44:41 +0100

    rna_align_sto2csv.py: + demo gmp.sto -> csv

--- rna_tools/tools/rna_alignment/test.sh
+++ rna_tools/tools/rna_alignment/test.sh
@@ -3,7 +3,15 @@
 #python rna_align_subcols.py test_data/u6_isl_rfam.sto 
 #python rna_align_foldability.py test_data/gmp_ref.sto test_data/gmp_foldability.csv --verbose
 
-python rna_align_coverage.py xrRNA test_data/xrrna_rfam.sto
+#python rna_align_coverage.py xrRNA test_data/xrrna_rfam.sto
 #easel alistat test_data/xrrna_rfam.sto
-python rna_align_coverage.py u6isl test_data/u6_isl_rfam.sto
+#python rna_align_coverage.py u6isl test_data/u6_isl_rfam.sto
 
+# calc rmsd
+#python rna_structure_rmsd.py test_data/1u6b.pdb test_data/1u6b_mod.pdb --chain A
+#python demo.py
+#python openfold3.py
+
+python rna_align_sto2csv.py test_data/gmp.sto -o test_data/gmp.csv
+head test_data/gmp_ref.csv
+#open .
